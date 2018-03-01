c28c8c28201ab83d08ba670e7cca7656e9bb88cc
dgketchum <dgketchum@gmail.com>
2018-02-28 18:53:42 -0700

    added binary_true option

--- core/softmax.py
+++ core/softmax.py
@@ -1,15 +1,26 @@
 import numpy as np
-from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import scale, normalize
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 
-def softmax_regression(data):
+def softmax_regression(data, binary_true=None):
 
     x = data['data']
-    y = data['target']
-    N = x.shape[1]
+    y_strings = data['target_values']
+
+    classes = data['classes']
+    unique, y = np.unique(y_strings, return_inverse=True)
+    class_counts = {x: list(y_strings).count(x) for x in classes}
+    print('Class counts: {}'.format(class_counts))
+    class_map = dict(zip(list(unique), [0, 1, 2, 3]))
+    print('Class integer map: {}'.format(class_map))
+
+    if binary_true:
+        y[y_strings == binary_true] = 1
+        y[y_strings != binary_true] = 0
+
+    N = len(classes)
     d, d_test, y, y_test = train_test_split(x, y, test_size=0.33,
                                             random_state=42)
     d = normalize(d)
@@ -31,7 +42,7 @@ def softmax_regression(data):
     for i in range(N_iterations):
         W -= eta * _gradient(X, W, T, m, N)
 
-    y_pred = np.argmax(_softmax(X, W, None), axis=1)
+    y_pred = np.argmax(_softmax(X, W, N), axis=1)
     print('Objective Function Value: ', j_function(X, W, T),
           'Total misclassified: ', sum(y != y_pred))
     print(confusion_matrix(y_pred, y))
@@ -39,19 +50,19 @@ def softmax_regression(data):
 
     X = np.column_stack((np.ones_like(y_test), d_test))
 
-    y_test_pred = np.argmax(_softmax(X, W, None), axis=1)
+    y_test_pred = np.argmax(_softmax(X, W, N), axis=1)
 
     print(confusion_matrix(y_test_pred, y_test))
     print(accuracy_score(y_test, y_test_pred))
 
 
 def j_function(X, W, T):
-    return -np.sum(np.sum(T * np.log(_softmax(X, W, None)), axis=1), axis=0)
+    return -np.sum(np.sum(T * np.log(_softmax(X, W, T)), axis=1), axis=0)
 
 
 def _gradient(X, W, T, m, N):
     return -np.column_stack(
-        [np.sum([(T - _softmax(X, W, None))[i, k] * X[i] for i in range(m)], axis=0) for k in range(N)])
+        [np.sum([(T - _softmax(X, W, N))[i, k] * X[i] for i in range(m)], axis=0) for k in range(N)])
 
 
 def _softmax(X, W, N):
