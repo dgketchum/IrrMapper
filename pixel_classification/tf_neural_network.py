032953bfbd59545b7afe37118b49212ee63b06e5
David Ketchum <dgketchum@gmail.com>
2018-03-13 16:36:55 -0600

    tf_neural_net running 98.4% accuracy on test set

--- pixel_classification/tf_neural_network.py
+++ pixel_classification/tf_neural_network.py
@@ -33,17 +33,18 @@ def neural_net(data):
     n = data.x.shape[1]
     nodes = 500
     eta = 0.05
-    epochs = 300
+    epochs = 1000
     seed = 128
 
     data.x, x_test, data.y, y_test = train_test_split(data.x, data.y, test_size=0.33,
                                                       random_state=None)
 
     data.y = get_dummies(data.y).values
+    y_test = get_dummies(y_test).values
     X = tf.placeholder("float", [None, n])
     Y = tf.placeholder("float", [None, N])
 
-    batch_size = 10
+    batch_size = 100
 
     weights = {
         'hidden': tf.Variable(tf.random_normal([n, nodes], seed=seed)),
@@ -59,12 +60,13 @@ def neural_net(data):
 
     optimizer = tf.train.AdamOptimizer(learning_rate=eta).minimize(loss_op)
 
+    correct_pred = tf.equal(tf.argmax(Y, 1), tf.argmax(y_pred, 1))
+
     sess = tf.InteractiveSession()
     init = tf.global_variables_initializer().run()
     loss = None
 
     for step in range(epochs):
-
         offset = np.random.randint(0, data.y.shape[0] - batch_size - 1)
 
         batch_data = data.x[offset:(offset + batch_size), :]
@@ -76,16 +78,10 @@ def neural_net(data):
                            feed_dict=feed_dict)
 
         if step % 100 == 0:
-            print("Minibatch loss at step {0}: {1}".format(step, loss))
-            print("Minibatch accuracy: {:.1f}%".format(
-                tf.metrics.accuracy(batch_labels, data.y)))
-
-    pred = tf.nn.softmax(y_test)
-    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y_test, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    print("Accuracy:", accuracy.eval({X: x_test, Y: y_test}), loss)
-    print("\nTest accuracy: {:.1f}%".format(
-        tf.metrics.accuracy(y_test, pred)))
+            pred = tf.nn.softmax(y_pred)
+            correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            print("Accuracy:", accuracy.eval({X: x_test, Y: y_test}), loss)
 
 
 # Create neural network
