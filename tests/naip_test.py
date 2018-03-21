8c7c53fcece8ec5eb279215d97f3fd6233eceab5
David Ketchum <dgketchum@gmail.com>
2018-03-21 13:43:32 -0600

    working out NAIP projection issues

--- tests/naip_test.py
+++ tests/naip_test.py
@@ -16,13 +16,23 @@
 
 import unittest
 
+from convnet.naip import ApfoNaip
 
-class MyTestCase(unittest.TestCase):
+
+class AfpoNaipTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.tile_size = (512, 512)
+        self.box = (-110.129588, 46.405228, -110.0812563, 46.4365153)
+        self.test_tif = 'data/test_apfonaip.tif'
+        self.dst_srs = 32612
+        self.dstsrs_str = str(self.dst_srs)
+        self.profile = dict([('bboxSR', 4326), ('imageSR', self.dst_srs)])
 
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_image_reference(self):
+        naip = ApfoNaip(self.box, **self.profile)
+        array, profile = naip.get_image('montana', self.tile_size)
+        naip.save(array, profile, self.test_tif, crs=self.dst_srs)
+        pass
 
 
 if __name__ == '__main__':
