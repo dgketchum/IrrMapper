71efb38bdf210ed3f881e46fb88b950ebe97053c
David Ketchum <dgketchum@gmail.com>
2018-03-22 10:11:06 -0600

    added mt sp

--- spatial/bounds.py
+++ spatial/bounds.py
@@ -29,6 +29,14 @@ class BBox(object):
 
         return w, s, e, n
 
+    def to_mt_sp(self):
+        in_proj = Proj('+proj=lcc +lat_1=45 +lat_2=49 +lat_0=44.25 +'
+                       'lon_0=-109.5 +x_0=600000 +y_0=0 +ellps=GRS80 +units=m +no_defs')
+        w, s = in_proj(self.west, self.south)
+        e, n = in_proj(self.east, self.north)
+
+        return w, s, e, n
+
 
 class GeoBounds(BBox):
     """Spatial bounding box
