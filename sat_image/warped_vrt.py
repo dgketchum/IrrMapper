cfedb35111ebe3a5c3ca4af6c4593b5903cba15d
David Ketchum <dgketchum@gmail.com>
2018-03-26 19:46:40 -0600

    warped vrt running

--- sat_image/warped_vrt.py
+++ sat_image/warped_vrt.py
@@ -22,12 +22,12 @@ import rasterio
 from rasterio.enums import Resampling
 from rasterio.vrt import WarpedVRT
 from rasterio import shutil as rio_shutil
+from rasterio import open as rasopen
 
 from sat_image.image import Landsat5, Landsat7, Landsat8
 
 
 def warp_vrt(directory, sat):
-
     list_dir = os.listdir(directory)
     first = True
 
@@ -36,35 +36,45 @@ def warp_vrt(directory, sat):
 
             mapping = {'LC8': Landsat8, 'LE7': Landsat7, 'LT5': Landsat5}
 
-            landsat = mapping[sat](d)
-            vrt_options = landsat.rasterio_geometry
+            landsat = mapping[sat](os.path.join(directory, d))
+            dst = landsat.rasterio_geometry
 
-            vrt_options['resampling'] = Resampling.cubic
+            vrt_options = {'resampling': Resampling.cubic,
+                           'dst_crs': dst['crs'],
+                           'dst_transform': dst['transform'],
+                           'dst_height': dst['height'],
+                           'dst_width': dst['width']}
+
+            print('geometry: {} \n {}'.format(d, vrt_options))
+            print('shape: {}'.format(landsat.shape))
+
             first = False
 
         else:
             paths = []
-            band_mapping = {'LC8': [3, 4, 5, 10], 'LE7': [2, 3, 4, 6]}
-            for x in os.listdir(d):
+            band_mapping = {'LC8': ['3', '4', '5', '10'],
+                            'LE7': ['2', '3', '4', '6_VCID_2']}
+            for x in os.listdir(os.path.join(directory, d)):
                 for y in band_mapping[sat]:
                     if x.endswith('B{}.TIF'.format(y)):
-                        paths.append(os.path.join(d, x))
+                        paths.append(os.path.join(directory, d, x))
 
             for path in paths:
-
-                with rasterio.open(path) as src:
-
+                print(path)
+                with rasopen(path, 'r+') as src:
+                    src.crs = dst['crs']
                     with WarpedVRT(src, **vrt_options) as vrt:
-
                         data = vrt.read()
 
-                        directory, name = os.path.split(path)
-                        outfile = os.path.join(directory, 'aligned-{}'.format(name))
+                        dst_dir, name = os.path.split(path)
+                        print(dst_dir, name)
+                        outfile = os.path.join(dst_dir, name.replace('_T', '_TA'))
                         rio_shutil.copy(vrt, outfile, driver='GTiff')
 
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
-
+    images = os.path.join(home, 'landsat_images', 'LE7_39_27')
+    warp_vrt(images, 'LE7')
 
 # ========================= EOF ================================================================
