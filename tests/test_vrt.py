46079376c667ac9f9f33ad64feae4141efcb1a5d
David Ketchum <dgketchum@gmail.com>
2018-04-02 14:56:10 -0600

    added delete extra parameter

--- tests/test_vrt.py
+++ tests/test_vrt.py
@@ -30,7 +30,7 @@ class MyTestCase(unittest.TestCase):
         shutil.copytree(self.origin, self.directory)
 
     def test_warped_vrt(self):
-        warped_vrt.warp_vrt(self.directory, self.satellite, delete_extras=False)
+        warped_vrt.warp_vrt(self.directory, self.satellite)
         shapes = []
         dirs = [_ for _ in os.listdir(self.directory) if not _.endswith('.txt')]
         for d in dirs:
