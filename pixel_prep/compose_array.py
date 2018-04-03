6d0ac44022f905573cdde9d9ffd061ae515a6749
David Ketchum <dgketchum@gmail.com>
2018-04-03 16:53:53 -0600

    making pixel prep a class, working on make array

--- pixel_prep/compose_array.py
+++ pixel_prep/compose_array.py
@@ -27,7 +27,6 @@ from rasterio import open as rasopen
 from shapely.geometry import shape, Polygon, Point, mapping
 from shapely.ops import unary_union
 
-from pixel_prep.nlcd_map import map_nlcd_to_flu, nlcd_value
 from sat_image.image import LandsatImage, Landsat5, Landsat7, Landsat8
 
 WRS_2 = pkg_resources.resource_filename('spatial_data', 'wrs2_descending.shp')
@@ -40,16 +39,31 @@ function `compose_data_array` will return a numpy.ndarray object ready for a lea
 
 
 class PixelTrainingArray(object):
-    def __init__(self, training_shape, images, **kwargs):
-        mapping = {'LT5': Landsat5, 'LE7': Landsat7, 'LC8': Landsat8}
+    def __init__(self, training_shape, images):
+
+        self.save_points_shape = None
+        self.sample_points = None
+        self.extracted_points = None
+        self.data_dict = None
+
+        landsat_map = {'LT5': Landsat5, 'LE7': Landsat7, 'LC8': Landsat8}
         dirs = [os.path.join(images, x) for x in os.listdir(images) if os.path.isdir(os.path.join(images, x))]
         objs = [LandsatImage(os.path.join(images, x)).satellite for x in dirs]
-        self.images = [mapping[x](y) for x, y in zip(objs, dirs)]
 
+        self.images = [landsat_map[x](y) for x, y in zip(objs, dirs)]
         self.landsat = self.images[0]
         self.vectors = training_shape
 
-    def sample_coverage(self, points=10000, save_points=False):
+        self.tile_bbox, self.tile_meta, self.tile_polys, self.tile_area = self.tile_geometry
+
+    def extract_to_sample(self, points=10000, save_points=False):
+        self.sample_points = points
+        self.save_points_shape = save_points
+
+        self.extracted_points = self.sample_coverage()
+        self.data_dict = self.make_data_array()
+
+    def sample_coverage(self, points=10000):
         """ Create a clipped training set and inverse training set from polygon shapefiles.
 
         This complicated-looking function finds the wrs_2 descending Landsat tile corresponding
@@ -62,24 +76,14 @@ class PixelTrainingArray(object):
         """
 
         time = datetime.now()
-        bbox, meta = get_tile_geometry(self.path, self.row)
 
-        with fopen(self.training_shape, 'r') as src:
-            clipped = src.filter(mask=bbox)
-            polys = []
-            total_area = 0.
-            for feat in clipped:
-                geo = shape(feat['geometry'])
-                polys.append(geo)
-                total_area += geo.area
-
-        union = unary_union(polys)
+        union = unary_union(self.tile_polys)
         point_collection = {}
         interior_rings_dissolved = []
         obj_id = 1
         for poly in union:
             interior_rings_dissolved.append(poly.exterior.coords)
-            fractional_area = poly.area / total_area
+            fractional_area = poly.area / self.tile_area
             required_points = round(fractional_area * points * 0.5)
             min_x, max_x = min(poly.bounds[0]), max(poly.bounds[2])
             min_y, max_y = min(poly.bounds[1]), max(poly.bounds[3])
@@ -99,17 +103,17 @@ class PixelTrainingArray(object):
                 else:
                     break
 
-        shell = bbox['coordinates'][0]
+        shell = self.tile_bbox['coordinates'][0]
         inverse_polygon = Polygon(shell=shell, holes=interior_rings_dissolved)
         inverse_polygon = inverse_polygon.buffer(0)
         inverse_polygon = unary_union(inverse_polygon)
         coords = inverse_polygon.bounds
         min_x, max_x = coords[0], coords[2]
         min_y, max_y = coords[1], coords[3]
-        x_range = linspace(min_x, max_x, num=2 * points)
-        y_range = linspace(min_y, max_y, num=2 * points)
+        x_range = linspace(min_x, max_x, num=2 * self.sample_points)
+        y_range = linspace(min_y, max_y, num=2 * self.sample_points)
         shuffle(x_range), shuffle(y_range)
-        required_points = round(points * 0.5)
+        required_points = round(self.sample_points * 0.5)
         count = 0
         time = datetime.now()
         for coord in zip(x_range, y_range):
@@ -129,25 +133,74 @@ class PixelTrainingArray(object):
 
         print('Total area in decimal degrees: {}\n'
               'Area irrigated: {}\n'
-              'Fraction irrigated: {}'.format(shape(bbox).area, total_area,
-                                              total_area / shape(bbox).area))
-        if save_points:
-
-            points_schema = {'properties': dict(
-                [('OBJECTID', 'int:10'), ('POINT_TYPE', 'int:10')]),
-                'geometry': 'Point'}
-
-            meta['schema'] = points_schema
-
-            with fopen(temp_points, 'w', **meta) as output:
-                for key, val in point_collection.items():
-                    props = dict([('OBJECTID', key), ('POINT_TYPE', val['POINT_TYPE'])])
-                    pt = Point(val['COORDS'][0], val['COORDS'][1])
-                    output.write({'properties': props,
-                                  'geometry': mapping(pt)})
-
-        print('sample operation on {} points in {} seconds'.format(points,
+              'Fraction irrigated: {}'.format(shape(self.tile_bbox).area, self.tile_area,
+                                              self.tile_area / shape(self.tile_bbox).area))
+        print('Sample operation on {} points in {} seconds'.format(self.sample_points,
                                                                    (datetime.now() - time).seconds))
+
+        return point_collection
+
+    def make_data_array(self):
+
+        df = DataFrame()
+
+        for i in self.images:
+            for band, path in i.tif_dict:
+                band_series = self.point_raster_extract(path, df)
+                df = df.join(band_series, how='outer')
+
+        target_series = Series(df.LTYPE)
+        target_values = target_series.values
+
+        data = {'features': df.columns.values, 'data': df.values,
+                'target_values': target_values}
+
+        if pickle_path:
+            with open(pickle_path, 'wb') as handle:
+                pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        return data
+
+    def point_raster_extract(self, raster, points):
+
+        basename = os.path.basename(raster)
+        name_split = basename.split(sep='_')
+        band = name_split[7].split(sep='.')[0]
+        date_string = name_split[3]
+        column_name = '{}_{}'.format(date_string, band)
+        print('pixel_prep {}'.format(column_name))
+
+        with rasopen(raster, 'r') as rsrc:
+            rass_arr = rsrc.read()
+            rass_arr = rass_arr.reshape(rass_arr.shape[1], rass_arr.shape[2])
+            affine = rsrc.affine
+
+        s = Series(index=range(0, points.shape[0]), name=column_name)
+        for ind, row in points.iterrows():
+            x, y = row['X'], row['Y']
+            c, r = ~affine * (x, y)
+            try:
+                raster_val = rass_arr[int(r), int(c)]
+                s[ind] = float(raster_val)
+            except IndexError:
+                s[ind] = None
+
+        return s
+
+    def save_sample_points(self, point_collection):
+
+        points_schema = {'properties': dict(
+            [('OBJECTID', 'int:10'), ('POINT_TYPE', 'int:10')]),
+            'geometry': 'Point'}
+        meta = self.tile_meta.copy()
+        meta['schema'] = points_schema
+
+        with fopen(temp_points, 'w', **meta) as output:
+            for key, val in point_collection.items():
+                props = dict([('OBJECTID', key), ('POINT_TYPE', val['POINT_TYPE'])])
+                pt = Point(val['COORDS'][0], val['COORDS'][1])
+                output.write({'properties': props,
+                              'geometry': mapping(pt)})
 
     @property
     def tile_geometry(self):
@@ -157,183 +210,22 @@ class PixelTrainingArray(object):
                 fp = feature['properties']
                 if fp['PATH'] == self.path and fp['ROW'] == self.row:
                     bbox = feature['geometry']
-                    return bbox, wrs_meta
 
+        with fopen(self.vectors, 'r') as src:
+            clipped = src.filter(mask=bbox)
+            polys = []
+            total_area = 0.
+            for feat in clipped:
+                geo = shape(feat['geometry'])
+                polys.append(geo)
+                total_area += geo.area
+                return bbox, wrs_meta, polys, total_area
 
-def make_data_array(shapefile, rasters, pickle_path=None,
-                    nlcd_path=None, target_shapefiles=None, count=10000):
-    """ Compose numpy.ndarray prepped for a learning algorithm.
-    
-    
-    Keyword Arguments:
-    :param count: 
-    :param target_shapefiles: 
-    :param nlcd_path: 
-    :param pickle_path: 
-    :param rasters: 
-    :param shapefile: .shp file from which point locations will be taken.
-    :param rasters: Single pixel_prep file path, list of file paths, or a dir, from which all
-    /*.tif files will be used.
-    # :param transform: i.e., 'normalize', 'scale' data of real-number (continuous) variable
-    :return: numpy.ndarray
-    """
-
-    df = point_target_extract(points=shapefile, nlcd_path=nlcd_path, target_shapefile=target_shapefiles,
-                              count_limit=count)
-
-    rasters = raster_paths(rasters)
-    for r in rasters:
-        for b in ['3', '4', '5', '10']:
-            if r.endswith('B{}.TIF'.format(b)):
-                band_series = point_raster_extract(r, df)
-                df = df.join(band_series, how='outer')
-
-    target_series = Series(df.LTYPE)
-    map_nlcd_to_flu(target_series)
-    target_values = target_series.values
-    df.drop(['X', 'Y', 'ID', 'ITYPE', 'LTYPE'], inplace=True, axis=1)
-
-    data = {'features': df.columns.values, 'data': df.values,
-            'target_values': target_values}
-
-    if pickle_path:
-        with open(pickle_path, 'wb') as handle:
-            pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    return data
-
-
-def raster_paths(rasters):
-    """ Return list of rasters from single pixel_prep, list of rasters, or a dir.    """
-    if os.path.isfile(rasters):
-        return [rasters]
-    elif os.path.isfile(rasters[0]):
-        return rasters
-    elif os.path.isdir(rasters):
-        lst = list(recursive_file_gen(rasters))
-        return [x for x in lst if x.endswith('.TIF')]
-
-    else:
-        raise ValueError('Must provide a single .tif, a list of .tif files, or a dir')
-
-
-def recursive_file_gen(mydir):
-    for root, dirs, files in os.walk(mydir):
-        for file in files:
-            yield os.path.join(root, file)
-
-
-def point_target_extract(points, nlcd_path, target_shapefile=None,
-                         count_limit=None):
-    point_data = {}
-    with fopen(points, 'r') as src:
-        for feature in src:
-            name = feature['id']
-            proj_coords = feature['geometry']['coordinates']
-            point_data[name] = {'point': feature['geometry'],
-                                'coords': proj_coords}
-            # point_crs = src.profile['crs']['init']
-    pt_ct = 0
-    for pt_id, val in point_data.items():
-        pt_ct += 1
-        if pt_ct < count_limit:
-            pt = shape(val['point'])
-            with fopen(target_shapefile, 'r') as target_src:
-                has_attr = False
-                for t_feature in target_src:
-                    polygon = t_feature['geometry']
-                    if pt.within(shape(polygon)):
-                        print('pt id {}, props: {}'
-                              .format(pt_id, t_feature['properties']))
-                        props = t_feature['properties']
-                        point_data[pt_id]['properties'] = {'IType': props['IType'],
-                                                           'LType': props['LType']}
-
-                        has_attr = True
-                        break
-
-                if not has_attr:
-                    if nlcd_path:
-                        with rasopen(nlcd_path, 'r') as rsrc:
-                            rass_arr = rsrc.read()
-                            rass_arr = rass_arr.reshape(rass_arr.shape[1], rass_arr.shape[2])
-                            affine = rsrc.affine
-
-                            x, y = val['coords']
-                            col, row = ~affine * (x, y)
-                            raster_val = rass_arr[int(row), int(col)]
-                            ltype_dct = {'IType': None,
-                                         'LType': str(raster_val)}
-                            point_data[pt_id]['properties'] = ltype_dct
-                            print('id {} has no FLU, '
-                                  'nlcd {}'.format(pt_id,
-                                                   nlcd_value(ltype_dct['LType'])))
-                    else:
-                        ltype_dct = {'IType': None,
-                                     'LType': None}
-                        point_data[pt_id]['properties'] = ltype_dct
-
-    idd = []
-    ltype = []
-    itype = []
-    x = []
-    y = []
-    ct = 0
-    for pt_id, val in point_data.items():
-        ct += 1
-        if ct < count_limit:
-            idd.append(pt_id)
-            ltype.append(val['properties']['LType'])
-            itype.append(val['properties']['IType'])
-            x.append(val['coords'][0])
-            y.append(val['coords'][1])
-        else:
-            break
-    dct = dict(zip(['ID', 'LTYPE', 'ITYPE', 'X', 'Y'],
-                   [idd, ltype, itype, x, y]))
-    df = DataFrame(data=dct)
-
-    return df
-
-
-def point_raster_extract(raster, points):
-    """ Get point values from a pixel_prep.
-
-    :param raster: local_raster
-    :param points: Shapefile of points.
-    :return: Dict of coords, row/cols, and values of pixel_prep at that point.
-    """
-
-    basename = os.path.basename(raster)
-    name_split = basename.split(sep='_')
-    band = name_split[7].split(sep='.')[0]
-    date_string = name_split[3]
-    column_name = '{}_{}'.format(date_string, band)
-    print('pixel_prep {}'.format(column_name))
-
-    with rasopen(raster, 'r') as rsrc:
-        rass_arr = rsrc.read()
-        rass_arr = rass_arr.reshape(rass_arr.shape[1], rass_arr.shape[2])
-        affine = rsrc.affine
-
-    s = Series(index=range(0, points.shape[0]), name=column_name)
-    for ind, row in points.iterrows():
-        x, y = row['X'], row['Y']
-        c, r = ~affine * (x, y)
-        try:
-            raster_val = rass_arr[int(r), int(c)]
-            s[ind] = float(raster_val)
-        except IndexError:
-            s[ind] = None
-
-    return s
-
-
-def _point_attrs(pt_data, index):
-    target = Series(name='target', index=index)
-    for key, val in pt_data.items():
-        target.iloc[int(key)] = pt_data[key]['land_type']
-    return target
+    @staticmethod
+    def _recursive_file_gen(mydir):
+        for root, dirs, files in os.walk(mydir):
+            for file in files:
+                yield os.path.join(root, file)
 
 
 if __name__ == '__main__':
