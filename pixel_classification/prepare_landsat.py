a48408ccb1f7c77b872a57de4cc50f0109005402
David Ketchum <dgketchum@gmail.com>
2018-04-07 12:48:28 -0600

    dir tree creation working

--- pixel_classification/prepare_landsat.py
+++ pixel_classification/prepare_landsat.py
@@ -28,9 +28,15 @@ def prepare_image_stack(path, row, year, credentials, outpath, satellite='LC8'):
     start, end = '{}-05-01'.format(year), '{}-10-15'.format(year)
     start = datetime.strptime(start, '%Y-%m-%d')
     end = datetime.strptime(end, '%Y-%m-%d')
-    download_composer.download_landsat(start, end, satellite, path, row, outpath, credentials, max_cloud_percent=20)
+
+    sub_directory = orgainize_directory(outpath, path, row, year)
+
+    download_composer.download_landsat(start, end, satellite=satellite, path=path, row=row, output_path=sub_directory,
+                                       usgs_creds=credentials, max_cloud_percent=20)
     make_fmask(outpath, sat=satellite)
     warp_vrt(outpath, sat=satellite, delete_extra=True, use_band_map=True)
+
+    return None
 
 
 def make_fmask(image_dir, sat='LC8'):
@@ -54,33 +60,28 @@ def make_fmask(image_dir, sat='LC8'):
         f.save_array(water, dst_path_water)
 
 
-def orgainize_directory(pymetric_rt, paths, rows, years):
-    root_list = os.listdir(pymetric_rt)
-    landsat = os.path.join(pymetric_rt, 'landsat')
-    if 'landsat' not in root_list:
-        print('Making landsat dir in {}'.format(pymetric_rt))
-        os.mkdir(landsat)
-    for path in paths:
-        for row in rows:
-            for year in years:
-                dst_dir = os.path.join(landsat, str(path), str(row),
-                                       str(year))
-                try:
-                    os.makedirs(dst_dir)
-                    print('Made {}'.format(dst_dir))
-                except:
-                    pass
+def orgainize_directory(root, path, row, year):
+    dst_dir = os.path.join(root, str(path), str(row),
+                           str(year))
+    if not os.path.isdir(dst_dir):
+        try:
+            os.makedirs(dst_dir)
+            print('Made {}'.format(dst_dir))
+
+        except:
+            pass
+
+    return dst_dir
 
 
 # TODO: drop rows under cloud from PixelTrainingArray
-# TODO: use pymetric function to create proper directory tree under master root (i.e., 'landsat_data')
 
 if __name__ == '__main__':
     p, r = 39, 27
     yr = 2015
     home = os.path.expanduser('~')
     creds = os.path.join(home, 'usgs.txt')
-    images = 'landsat_data'
+    images = os.path.dirname(__file__).replace('pixel_classification', 'landsat_data')
     prepare_image_stack(p, r, yr, creds, images, satellite='LC8')
 
 # ========================= EOF ====================================================================
