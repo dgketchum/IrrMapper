8fa2f3b9c2e3b7314b231f45a1421a8580a3a733
David Ketchum <dgketchum@gmail.com>
2018-04-07 12:16:33 -0600

    adding directory organization to prepare_landsat

--- pixel_classification/prepare_landsat.py
+++ pixel_classification/prepare_landsat.py
@@ -21,33 +21,16 @@ from datetime import datetime
 from landsat import download_composer
 from sat_image.image import Landsat5, Landsat7, Landsat8
 from sat_image.fmask import Fmask
+from sat_image.warped_vrt import warp_vrt
 
 
-def get_image(path, row, start='2015-05-01', end='2015-10-30', usgs_credentials=None,
-              output_path=None, satellite='all'):
-    """ Get Landsat data from USGS site.
-
-    :param path: int path from WRS2 reference system
-    :param row: Row
-    :param start: start date of image search; ftm = 'YYY-MM-DD'
-    :param end: end date of image search
-    :param usgs_credentials: path to .txt file with <username> <password> separated by one space
-    :param output_path: location
-    :param satellite: 'all' will result in search of LT5, LE7, and LC8, otherwise choose one of the three
-    :return: None
-    """
-
+def prepare_image_stack(path, row, year, credentials, outpath, satellite='LC8'):
+    start, end = '{}-05-01'.format(year), '{}-10-15'.format(year)
     start = datetime.strptime(start, '%Y-%m-%d')
     end = datetime.strptime(end, '%Y-%m-%d')
-    if satellite == 'all':
-        for sat in ['LT5', 'LE7', 'LC8']:
-            download_composer.download_landsat(start, end, sat, path=path,
-                                               row=row, output_path=output_path,
-                                               usgs_creds=usgs_credentials)
-    else:
-        download_composer.download_landsat(start, end, satellite, path=path,
-                                           row=row, output_path=output_path,
-                                           usgs_creds=usgs_credentials)
+    download_composer.download_landsat(start, end, satellite, path, row, outpath, credentials, max_cloud_percent=20)
+    make_fmask(outpath, sat=satellite)
+    warp_vrt(outpath, sat=satellite, delete_extra=True, use_band_map=True)
 
 
 def make_fmask(image_dir, sat='LC8'):
@@ -71,17 +54,33 @@ def make_fmask(image_dir, sat='LC8'):
         f.save_array(water, dst_path_water)
 
 
+def orgainize_directory(pymetric_rt, paths, rows, years):
+    root_list = os.listdir(pymetric_rt)
+    landsat = os.path.join(pymetric_rt, 'landsat')
+    if 'landsat' not in root_list:
+        print('Making landsat dir in {}'.format(pymetric_rt))
+        os.mkdir(landsat)
+    for path in paths:
+        for row in rows:
+            for year in years:
+                dst_dir = os.path.join(landsat, str(path), str(row),
+                                       str(year))
+                try:
+                    os.makedirs(dst_dir)
+                    print('Made {}'.format(dst_dir))
+                except:
+                    pass
+
+
+# TODO: drop rows under cloud from PixelTrainingArray
+# TODO: use pymetric function to create proper directory tree under master root (i.e., 'landsat_data')
+
 if __name__ == '__main__':
+    p, r = 39, 27
+    yr = 2015
     home = os.path.expanduser('~')
     creds = os.path.join(home, 'usgs.txt')
-    images = os.path.join(home, 'landsat_images', 'LC8_39_27')
-    dirs = os.listdir(images)
-    for image in dirs:
-        path = os.path.join(images, image)
-        print(path)
-        make_fmask(image_dir=path)
-        # get_image(39, 27, '2015-05-01', '2015-10-30',
-        #           output_path=out, satellite='LC8',
-        #           usgs_credentials=creds)
+    images = 'landsat_data'
+    prepare_image_stack(p, r, yr, creds, images, satellite='LC8')
 
 # ========================= EOF ====================================================================
