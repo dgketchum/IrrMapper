daac9070df6faa1dbd6938cc3b0f47efa27462fa
dgketchum <dgketchum@gmail.com>
2018-07-14 18:14:34 -0600

    adding function to build compiled feature array

--- pixel_classification/runner.py
+++ pixel_classification/runner.py
@@ -16,6 +16,8 @@
 
 import os
 from pixel_classification.runspec import Idaho, Montana, Nevada, Oregon, Washington
+from pixel_classification.prepare_landsat import prepare_image_stack
+from pixel_classification.compose_array import PixelTrainingArray
 
 home = os.path.expanduser('~')
 ROOT = os.path.join(home, 'IrrigationGIS', 'western_states_irrgis')
@@ -33,6 +35,10 @@ def build_compiled_feature_array():
         path = os.path.join(ROOT, key)
         geo = obj(path)
 
+        prepare_image_stack(geo.path, geo.row, geo.year, path, geo.sat)
+
+        p = PixelTrainingArray(path, instances=1000, overwrite_existing=True, geography=geo)
+        p.extract_sample(save_points=True, limit_sample=False)
 
 
 if __name__ == '__main__':
