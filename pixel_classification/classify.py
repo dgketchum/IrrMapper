91051ed52ffe44905296d56e734c57332462b028
dgketchum <dgketchum@gmail.com>
2018-08-07 13:18:10 -0600

    move raster classifier to seperate classification script

--- pixel_classification/classify.py
+++ pixel_classification/classify.py
@@ -1,7 +1,7 @@
-# =============================================================================================
+# ===============================================================================
 # Copyright 2018 dgketchum
 #
-# Licensed under the Apache License, Version 2.LE07_clip_L1TP_039027_20150529_20160902_01_T1_B1.TIF (the "License");
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -12,36 +12,88 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# =============================================================================================
+# ===============================================================================
 
 import os
+import numpy as np
+import tensorflow as tf
+import rasterio.open as rasopen
+from rasterio.dtypes import float32
+from datetime import datetime
 
-from pixel_classification.compose_array import PixelTrainingArray
-from pixel_classification.tf_multilayer_perceptron import mlp
+from numpy import zeros, array
+from numpy.ma import array as marray
+from sklearn.preprocessing import StandardScaler
+
+from pixel_classification.tf_multilayer_perceptron import multilayer_perceptron
 
 
-def build_model(data, model=None):
-    if isinstance(data, PixelTrainingArray):
-        pass
+def classify_raster(data, out_location=None):
 
-    elif os.path.isfile(data):
-        data = PixelTrainingArray(pickle_path=data)
+    features = data.features.tolist()
+    stack = None
+    first = True
+    arr = None
+    for i, feat in enumerate(features):
+        with rasopen(data.model_map[feat], mode='r') as src:
+            arr = src.read()
+            meta = src.meta.copy()
+        if first:
+            empty = zeros((len(features), arr.shape[1], arr.shape[2]), float16)
+            stack = empty
+            stack[i, :, :] = normalize_image_channel(arr)
 
-    else:
-        raise TypeError('Classification requires PixelTrainingArray object.')
+            first = False
+        else:
+            stack[i, :, :] = normalize_image_channel(arr)
 
-    mlp(data, model)
+    final_shape = 1, stack.shape[1], stack.shape[2]
+    stack = stack.reshape((stack.shape[0], stack.shape[1] * stack.shape[2]))
+    stack[stack == 0.] = np.nan
+    m_stack = marray(stack, mask=np.isnan(stack))
 
+    new_array = np.zeros_like(arr.reshape((1, arr.shape[1] * arr.shape[2])), dtype=float16)
+
+    pixel = tf.placeholder("float", [None, n])
+    classify = tf.add(tf.matmul(multilayer_perceptron(pixel, weights['hidden'], biases['hidden']),
+                                weights['output']), biases['output'])
+    time = datetime.now()
+
+    ct_nan = 0
+    ct_out = 0
+
+    for i in range(m_stack.shape[-1]):
+        if not np.ma.is_masked(m_stack[:, i]):
+            dat = m_stack[:, i]
+            dat = array(dat).reshape((1, dat.shape[0]))
+            loss = sess.run(classify, feed_dict={pixel: dat})
+            new_array[0, i] = np.argmax(loss, 1)
+            ct_out += 1
+        else:
+            new_array[0, i] = np.nan
+            ct_nan += 1
+
+        if i % 1000000 == 0:
+            print('Count {} of {} pixels in {} seconds'.format(i, m_stack.shape[-1],
+                                                               (datetime.now() - time).seconds))
+
+    new_array = new_array.reshape(final_shape)
+    new_array = array(new_array, dtype=float32)
+
+    meta['count'] = 1
+    meta['dtype'] = float32
+    with rasopen(os.path.join(out_location, 'binary_raster.tif'), 'w', **meta) as dst:
+        dst.write(new_array)
     return None
 
+def normalize_image_channel(data):
+    data = data.reshape((data.shape[1], data.shape[2]))
+    scaler = StandardScaler()
+    scaler = scaler.fit(data)
+    data = scaler.transform(data)
+    data = data.reshape((1, data.shape[0], data.shape[1]))
+    return data
 
 if __name__ == '__main__':
-    home = os.path.expanduser('~')
-    root = os.path.dirname(__file__).replace('pixel_classification', 'landsat_data')
-
-    year = os.path.join(root, '39', '27', '2015')
-    p_path = os.path.join(year, 'data.pkl')
-
-    build_model(p_path, model=year)
-
+    pass
 # ========================= EOF ====================================================================
