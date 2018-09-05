df92e73f47b6b3dfbed5c385f3d0539d7b02ad42
dgketchum <dgketchum@gmail.com>
2018-09-05 16:55:04 -0600

    refactoring build image stack into a class

--- pixel_classification/prepare_landsat.py
+++ pixel_classification/prepare_landsat.py
@@ -24,85 +24,113 @@ from sat_image.warped_vrt import warp_vrt
 from sat_image.bounds import RasterBounds
 from dem import AwsDem
 
-MAPPING_OBJECTS = {'LT5': Landsat5, 'LE7': Landsat7, 'LC8': Landsat8}
-MAPPING_ABV = {5: 'LT5', 7: 'LE7', 8: 'LC8'}
 
+class ImageStack(object):
+    """
+    Prepare a stack of images from Landsat, terrain, etc. Save stack in identical geometry.
+    """
 
-def prepare_image_stack(path, row, year, outpath, satellite=8, skip_landsat=False):
-    start, end = '{}-05-01'.format(year), '{}-10-15'.format(year)
-    year_dir = os.path.join(outpath, str(path), str(row))
-    sub_directory = orgainize_directory(outpath, path, row, year)
-    print('preparing landsat at: {}'.format(sub_directory))
+    def __init__(self, satellite, path, row, max_cloud_pct=None, start=None, end=None,
+                 year=None, output_directory=None):
 
-    if not skip_landsat:
-        g = GoogleDownload(start, end, satellite, path=path, row=row,
-                           output_path=sub_directory, max_cloud_percent=20)
+        self.landsat_mapping = {'LT5': Landsat5, 'LE7': Landsat7, 'LC8': Landsat8}
+        self.landsat_mapping_abv = {5: 'LT5', 7: 'LE7', 8: 'LC8'}
+
+        self.sat = satellite
+        self.sat_abv = self.landsat_mapping_abv[self.sat]
+        self.sat_obj = self.landsat_mapping[self.sat_abv]
+
+        self.path = path
+        self.row = row
+        self.year = year
+
+        self.max_cloud = max_cloud_pct
+        self.start = start
+        self.end = end
+        self.out_dir = output_directory
+
+        self.profile = None
+        self.image_dir = None
+        self.dst_path_cloud = None
+        self.dst_path_water = None
+        self.landsat = None
+        self.image_dirs = None
+        self.image_paths = None
+
+        if start and end:
+            self.start_dt = '{}-05-01'.format(self.year)
+            self.end_dt = '{}-10-15'.format(self.year)
+
+    def get_landsat(self):
+        g = GoogleDownload(self.start, self.end, self.sat, path=self.path, row=self.row,
+                           output_path=self.out_dir, max_cloud_percent=self.max_cloud)
         g.download()
 
-    dirs = [x[0] for x in os.walk(sub_directory) if os.path.basename(x[0])[:3] in MAPPING_OBJECTS.keys()]
+        self.image_dirs = [x[0] for x in os.walk(self.out_dir) if
+                           os.path.basename(x[0])[:3] in self.landsat_mapping.keys()]
 
-    master = dirs[0]
-    first = True
-    for d in dirs:
-        satellite_abv = MAPPING_ABV[satellite]
-        if first:
-            l8 = MAPPING_OBJECTS[satellite_abv](master)
-            dem_name = os.path.join(year_dir, 'dem.tif')
-            slope_name = os.path.join(year_dir, 'slope.tif')
-            if not os.path.isfile(dem_name):
-                polygon = l8.get_tile_geometry()
-                profile = l8.rasterio_geometry
-                bb = RasterBounds(affine_transform=profile['transform'],
-                                  profile=profile, latlon=True)
+        self.get_geography()
+        [self.make_fmask(d, sat=self.sat_abv) for d in self.image_dirs]
 
-                dem = AwsDem(zoom=10, target_profile=profile, bounds=bb, clip_object=polygon)
+    def get_geography(self):
 
-                dem.terrain(attribute='elevation',
-                            out_file=dem_name)
-                dem.terrain(attribute='slope',
-                            out_file=slope_name)
-            first = False
+        master = self.image_dirs[0]
+        self.landsat = self.landsat_mapping[self.sat_abv](master)
 
-        make_fmask(d, sat=satellite_abv)
+    def get_terrain(self):
 
-    warp_vrt(sub_directory, delete_extra=True, use_band_map=True)
+        slope_name = os.path.join(self.out_dir, 'slope.tif')
 
-    return None
+        if not os.path.isfile(slope_name):
+            polygon = self.landsat.get_tile_geometry()
+            self.profile = self.landsat.rasterio_geometry
 
+            bb = RasterBounds(affine_transform=self.profile['transform'],
+                              profile=self.profile, latlon=True)
 
-def make_fmask(image_dir, sat='LC8'):
-    dst_path_cloud = os.path.join(image_dir, 'cloud_fmask.tif')
-    dst_path_water = os.path.join(image_dir, 'water_fmask.tif')
+            dem = AwsDem(zoom=10, target_profile=self.profile, bounds=bb,
+                         clip_object=polygon)
 
-    if os.path.isfile(dst_path_cloud) and os.path.isfile(dst_path_water):
-        print('{} and {} exist'.format(os.path.basename(dst_path_cloud), os.path.basename(dst_path_water)))
+            dem.terrain(attribute='slope',
+                        out_file=slope_name)
 
-    else:
+        warp_vrt(sub_directory, delete_extra=True, use_band_map=True)
 
-        lst_image = MAPPING_OBJECTS[sat](image_dir)
+        return None
 
-        f = Fmask(lst_image)
+    def make_fmask(self, image_dir, sat='LC8'):
+        self.dst_path_cloud = os.path.join(image_dir, 'cloud_fmask.tif')
+        self.dst_path_water = os.path.join(image_dir, 'water_fmask.tif')
 
-        c, shadow, water = f.cloud_mask()
-        cloud = f.cloud_mask(cloud_and_shadow=True)
+        if os.path.isfile(self.dst_path_cloud) and os.path.isfile(self.dst_path_water):
+            print('{} and {} exist'.format(os.path.basename(self.dst_path_cloud),
+                                           os.path.basename(self.dst_path_water)))
 
-        f.save_array(cloud, dst_path_cloud)
-        f.save_array(water, dst_path_water)
+        else:
 
+            lst_image = self.landsat_mapping[sat](image_dir)
 
-def orgainize_directory(root, path, row, year):
-    dst_dir = os.path.join(root, str(path), str(row),
-                           str(year))
-    if not os.path.isdir(dst_dir):
+            f = Fmask(lst_image)
 
-        try:
-            os.makedirs(dst_dir)
-            print('Made {}'.format(dst_dir))
+            c, shadow, water = f.cloud_mask()
+            cloud = f.cloud_mask(cloud_and_shadow=True)
 
-        except Exception:
-            pass
+            f.save_array(cloud, self.dst_path_cloud)
+            f.save_array(water, self.dst_path_water)
 
-    return dst_dir
+    def orgainize_directory(self):
+        dst_dir = os.path.join(self.root, str(self.path), str(self.row),
+                               str(self.year))
+        if not os.path.isdir(dst_dir):
+
+            try:
+                os.makedirs(dst_dir)
+                print('Made {}'.format(dst_dir))
+
+            except Exception:
+                pass
+
+        return dst_dir
 
 
 if __name__ == '__main__':
