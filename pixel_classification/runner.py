9c7ae6eb92c5c80a7483f88c81fe334cd124be87
dgketchum <dgketchum@gmail.com>
2018-10-02 15:15:41 -0600

    set for two classes

--- pixel_classification/runner.py
+++ pixel_classification/runner.py
@@ -84,7 +84,7 @@ def model_training_scenes(project, n_images, training, model):
                        n_landsat=n_images, year=geo.year, max_cloud_pct=70)
         i.build_training()
         p = Pta(root=i.root, geography=geo, paths_map=i.paths_map, instances=10000, masks=i.masks,
-                overwrite_array=False, overwrite_points=False, pkl_path=geo_data_path)
+                overwrite_array=True, overwrite_points=True, pkl_path=geo_data_path)
         p.extract_sample()
 
         if first:
