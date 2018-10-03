548caf3aece60434707bf6692f79c06827ce7c48
dgketchum <dgketchum@gmail.com>
2018-10-03 15:44:19 -0600

    investigate CRC-less images causing fmask, dem problems

--- pixel_classification/prepare_images.py
+++ pixel_classification/prepare_images.py
@@ -151,7 +151,8 @@ class ImageStack(object):
         for i, d in enumerate(self.image_dirs):
             l = self.landsat_mapping[self.sat_abv](d)
             _id = l.landsat_scene_id
-            print('dir: {}\n {}'.format(d, os.listdir(d)))
+            print('dir: {}'.format(d))
+            [print(x) for x in os.listdir(d)]
             print('clip geo: {}'.format(l.get_tile_geometry()))
             get_image(image_dir=d, parent_dir=self.root, image_exists=True, image_id=_id,
                       satellite=self.sat, path=self.path, row=self.row, image_date=l.date_acquired,
