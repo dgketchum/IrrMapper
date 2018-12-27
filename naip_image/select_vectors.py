b304dee76e8e6d4a8ad9f57a349f43e3d0114a4c
dgketchum <dgketchum@gmail.com>
2018-12-27 11:13:55 -0700

    read shapefile, put selected fields into a pd.Series

--- naip_image/select_vectors.py
+++ naip_image/select_vectors.py
@@ -16,7 +16,27 @@
 
 import os
 
-if __name__ == '__main__':
+from pandas import DataFrame
+from geopandas import read_file
+
+
+def get_shapefile(shp, **filter_attrs):
+    gdf = read_file(shp)
+    df = DataFrame(gdf)
+    _drop = [x for x in df.columns if x not in filter_attrs['select']]
+    df.drop(columns=_drop, inplace=True)
+    df.sample(frac=1.)
+    return df['geometry']
+
+
+def visualize_geometries(geometries):
     pass
 
+
+if __name__ == '__main__':
+    home = os.path.expanduser('~')
+    tables = os.path.join(home, 'Field_Extract', 'training_tables')
+    o = os.path.join(tables, 'MT_test_table.csv')
+    s = os.path.join(home, 'IrrigationGIS', 'irr_gis', 'FLUS_All.shp')
+    get_shapefile(s, **{'field': 'LType', 'select': ['F', 'I']})
 # ========================= EOF ====================================================================
