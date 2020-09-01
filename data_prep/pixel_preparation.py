266ad233dace616096847e8e6e21b01c5120dbf7
dgketchum <pdWs7kyy>
2020-09-01 09:42:54 -0600

    write pixel sets to pixel data chunks

--- data_prep/pixel_preparation.py
+++ data_prep/pixel_preparation.py
@@ -2,8 +2,7 @@ import json
 import os
 import scipy.ndimage.measurements as mnts
 import warnings
-from numpy import zeros_like, array, newaxis, sum, where, nan, swapaxes, count_nonzero
-from numpy import nanmean, iinfo, uint32, sqrt, save, isnan, all, any, load, repeat
+import numpy as np
 import pickle as pkl
 
 # dates are generic, dates of each year as below, but data is from many years
@@ -26,7 +25,7 @@ DATES = {0: '19860101',
 CHANNELS = 7
 BANDS = 91
 
-structure = array([
+structure = np.array([
     [1, 1, 1],
     [1, 1, 1],
     [1, 1, 1]
@@ -34,7 +33,7 @@ structure = array([
 
 
 def write_pixel_set(out, recs):
-    """Iterate through each image of feature bands and labels, find contiguous objects, extract
+    """Iterate through each image of fenp.ature bands and labels, find contiguous objects, extract
     pixel set from within the objects
     """
     l = [os.path.join(recs, x) for x in os.listdir(recs)]
@@ -48,7 +47,7 @@ def write_pixel_set(out, recs):
     obj_ct = {0: 0, 1: 0, 2: 0, 3: 0}
     label_dict, size_dict, geom = {}, {}, {}
     for j, f in enumerate(l):
-        a = load(f)
+        a = np.load(f)
         labels = a[:, :, -4:]
         features = a[:, :, :-4]
 
@@ -60,29 +59,29 @@ def write_pixel_set(out, recs):
                 bbox_slices[i] = mnts.find_objects(mnts.label(lab, structure=structure)[0])
                 for b in bbox_slices[i]:
                     obj_ct[_class] += 1
-                    lab_mask = repeat(lab[b][:, :, newaxis], features.shape[-1], axis=2)
+                    lab_mask = np.repeat(lab[b][:, :, np.newaxis], features.shape[-1], axis=2)
                     nan_label = lab_mask.copy()
-                    nan_label[:, :, :] = iinfo(uint32).min
-                    c = where(lab_mask, features[b], nan_label)
-                    c[c == iinfo(uint32).min] = nan
+                    nan_label[:, :, :] = np.iinfo(np.uint32).min
+                    c = np.where(lab_mask, features[b], nan_label)
+                    c[c == np.iinfo(np.uint32).min] = np.nan
 
                     # geometric features (lat, lon, elev)
-                    geo = list(nanmean(c[:, :, -3:], axis=(0, 1)))
+                    geo = list(np.nanmean(c[:, :, -3:], axis=(0, 1)))
 
                     # pse.shape = T x C x S
                     c = c[:, :, :BANDS]
                     c = c.reshape(c.shape[0] * c.shape[1], BANDS)
-                    nan_mask = all(isnan(c), axis=1)
+                    nan_mask = np.all(np.isnan(c), axis=1)
                     c = c[~nan_mask]
                     c = c.reshape(c.shape[0], len(DATES.keys()), CHANNELS)
-                    c = swapaxes(c, 0, 2)
-                    c = swapaxes(c, 0, 1)
+                    c = np.swapaxes(c, 0, 2)
+                    c = np.swapaxes(c, 0, 1)
 
-                    if count_nonzero(isnan(geo)):
+                    if np.count_nonzero(np.isnan(geo)):
                         nan_geom += 1
                         continue
 
-                    if count_nonzero(isnan(c)):
+                    if np.count_nonzero(np.isnan(c)):
                         nan_pix += 1
                         continue
 
@@ -94,11 +93,11 @@ def write_pixel_set(out, recs):
 
                     # update mean and std
                     # mean_std.shape =  C x T
-                    delta = nanmean(c, axis=2) - mean_
+                    delta = np.nanmean(c, axis=2) - mean_
                     mean_ = mean_ + delta / count
-                    delta2 = nanmean(c, axis=2) - mean_
+                    delta2 = np.nanmean(c, axis=2) - mean_
                     M2 = M2 + delta * delta2
-                    std_ = sqrt(M2 / (count - 1))
+                    std_ = np.sqrt(M2 / (count - 1))
 
                     geom[count] = geo
                     label_dict[count] = _class
@@ -106,7 +105,7 @@ def write_pixel_set(out, recs):
                     if count % 100 == 0:
                         print('count: {}'.format(count))
 
-                    save(os.path.join(out, 'DATA', '{}'.format(count)), c)
+                    np.save(os.path.join(out, 'DATA', '{}'.format(count)), c)
 
     print('objects count: {}'.format(obj_ct))
     print('final pse shape: {}'.format(c.shape))
@@ -135,33 +134,46 @@ def write_pixel_set(out, recs):
     exit()
 
 
-def display_box(labels, slices):
-    """"Display bounding boxes with pyplot, pixel set will be extracted from within these boxes"""
-    from matplotlib import pyplot as plt
-    from matplotlib.colors import ListedColormap
+def write_pixel_blocks(data_dir, out, n_subset=100000):
+    """ write numpy arrays every n samples from pixel sets"""
 
-    cmap = ListedColormap(['grey', 'blue', 'purple', 'pink', 'green'])
+    np_data = os.path.join(data_dir, 'DATA')
+    meta_data = os.path.join(data_dir, 'META')
+    _files = [os.path.join(np_data, x) for x in os.listdir(np_data)]
+    with open(os.path.join(meta_data, 'labels.json'), 'r') as file:
+        label = json.loads(file.read())['label_4class']
+    label = [v for k, v in label.items()]
 
-    for n in range(labels.shape[-1]):
-        labels[:, :, n] *= n + 1
-
-    labels = sum(labels, axis=-1)
-    boxes = zeros_like(labels)
-
-    fig, ax = plt.subplots(ncols=2)
-    ax[0].imshow(labels, cmap=cmap)
-    for i, c in enumerate(slices.keys(), start=1):
-        for s in slices[c]:
-            boxes[s] = c + 1
-    ax[1].imshow(boxes, cmap=cmap)
-    plt.show()
+    first = True
+    count = 0
+    start_ind, end_ind = 0, 0
+    features, labels = None, []
+    for j, (f, l) in enumerate(zip(_files, label), start=1):
+        a = np.load(f)
+        labels.extend([l for _ in range(a.shape[-1])])
+        if first:
+            features = a
+            start_ind = count
+            first = False
+        else:
+            features = np.append(features, a, axis=-1)
+        count += a.shape[-1]
+        if features.shape[-1] > n_subset:
+            end_ind = count
+            np.save(os.path.join(out, '{}-{}'.format(start_ind, end_ind)), features)
+            np.save(os.path.join(out, '{}-{}_labels'.format(start_ind, end_ind)), np.array(labels))
+            print('{}-{}'.format(start_ind, end_ind))
+            features, labels = None, []
+            count += 1
+            first = True
+    print(count)
 
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
     parent = os.path.join(home, 'PycharmProjects', 'IrrMapper')
-    npy_recs = os.path.join(parent, 'data', 'npy')
+    pixel_dst = os.path.join(parent, 'data', 'pixels')
     pixel_sets = os.path.join(parent, 'data', 'pixel_sets')
-    write_pixel_set(pixel_sets, npy_recs)
+    write_pixel_blocks(pixel_sets, pixel_dst)
 
 # ========================= EOF ================================================================
