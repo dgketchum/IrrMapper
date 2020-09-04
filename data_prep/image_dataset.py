02f56e4456b5237c79591f81ebd71a1e6ba77bd9
dgketchum <pdWs7kyy>
2020-09-04 13:05:44 -0600

    prepare image dataset

--- data_prep/image_dataset.py
+++ data_prep/image_dataset.py
@@ -1,4 +1,3 @@
-
 import os
 
 import numpy as np
@@ -6,14 +5,18 @@ import numpy as np
 import torch
 from torch.utils import data
 
+from data_prep.pixel_preparation import BANDS, CHANNELS, DATES
+
 
 class ImageDataset(data.Dataset):
     def __init__(self, _dir, norm):
-
         super(ImageDataset, self).__init__()
 
         self.data_src = _dir
         self.norm = norm
+        self.sequence_len = len(DATES.keys())
+        self.channels = CHANNELS
+        self.bands = BANDS
 
         self.img_list = [os.path.join(_dir, x) for x in os.listdir(_dir) if x.endswith('.npy')]
         self.len = len(self.img_list)
@@ -22,20 +25,24 @@ class ImageDataset(data.Dataset):
         return self.len
 
     def __getitem__(self, item):
-
         a = np.load(self.img_list[item])
-        x = a[:, :, :-1]
-        y = a[:, :, -1:]
+
+        # needs B, T, C, H, W
+        x = a[:, :, :self.bands]
+        x = x.reshape(x.shape[0], x.shape[1], self.sequence_len, self.channels)
+        x = x.transpose((2, 3, 0, 1))
+
+        y = a[:, :, -4:]
+        y = torch.from_numpy(y).permute(2, 0, 1)
 
         if self.norm is not None:
             m, s = self.norm
-            m = np.array(m)
-            s = np.array(s)
+            m = np.array(m).reshape((self.sequence_len, self.channels, 1, 1))
+            s = np.array(s).reshape((self.sequence_len, self.channels, 1, 1))
             x = (x - m) / s
 
-        data = torch.from_numpy(x).float()
-
-        return data, torch.from_numpy(np.array(y, dtype=int))
+        x = torch.from_numpy(x).float()
+        return x, y
 
 
 if __name__ == '__main__':
