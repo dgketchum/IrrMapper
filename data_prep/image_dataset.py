ca17db954d99b39c7d7a708b78741d3feef1392b
dgketchum <pdWs7kyy>
2020-10-01 13:05:42 -0700

    transform tensor by mean and std

--- data_prep/image_dataset.py
+++ data_prep/image_dataset.py
@@ -1,27 +1,47 @@
 import os
 from itertools import islice
+import pickle as pkl
 import numpy as np
-from subprocess import Popen
 import torch
 from torch.utils import data
+from torchvision import transforms
 from webdataset import dataset as wds
 from webdataset import MultiDataset as wmds
 
 from data_prep.pixel_preparation import BANDS, CHANNELS, DATES
 from data_prep.bucket import get_bucket_contents
 
-def image_dataset(mode):
-    bucket = get_bucket_contents('ts_data')
-    gs_dir = 'cmask/tar/{}/{}_patches'.format(mode, mode)
-    tar_data = bucket[gs_dir]
-    url = 'http://storage.googleapis.com/ts_data/cmask/tar/train/train_patches/train_000001.tar'
-    url = f"pipe: curl -s -L {url} || true"
+mean_std = pkl.load(open('/home/dgketchum/IrrigationGIS/tfrecords/meanstd.pkl', 'rb'))
+
+normalize = transforms.Normalize(
+    mean=mean_std[0].reshape(mean_std[0].shape[0] * mean_std[0].shape[1]),
+    std=mean_std[1].reshape(mean_std[0].shape[0] * mean_std[0].shape[1]))
+
+transform_ = transforms.Compose([normalize])
+
+sequence_len = len(DATES.keys())
+
+
+def image_dataset():
+    # url = 'http://storage.googleapis.com/ts_data/cmask/tar/train/train_patches/train_000000.tar'
+    url = '/home/dgketchum/Downloads/train_000000.tar'
+    # url = f"pipe: curl -s -L {url} || true"
     bs = 20
-    dataset = wds.Dataset(url).decode('torchl').shuffle(100)
+    dataset = wds.Dataset(url).decode('torchl').map(map_fn)
     for sample in islice(dataset, 0, 3):
-        for key, value in sample.items():
-            print(key, repr(value)[:50])
-    return dataset
+        for s in sample:
+            print(s[0], s[1])
+
+
+def map_fn(item):
+    item = item['pth']
+    x = item[:, :, :BANDS]
+    x = x.permute(2, 0, 1)
+    x = transform_(x)
+    x = x.reshape(x.shape[1], x.shape[2], len(DATES.keys()), CHANNELS)
+    x = x.permute((2, 3, 0, 1))
+    y = item[:, :, -4:].permute(2, 0, 1)
+    return x, y
 
 
 class ImageDataset(data.Dataset):
@@ -64,5 +84,5 @@ class ImageDataset(data.Dataset):
 
 
 if __name__ == '__main__':
-    image_dataset('train')
+    image_dataset()
 # ========================= EOF ====================================================================
