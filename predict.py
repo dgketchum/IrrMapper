7f9f47f0f7f954c79b16b42e71ffd19f9a64af70
dgketchum <pdWs7kyy>
2020-10-09 12:24:50 -0600

    add unnormalize function to recover original image to compare against labels

--- predict.py
+++ predict.py
@@ -1,11 +1,32 @@
 import os
+import pickle as pkl
+
 import torch
+import numpy as np
 
 from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
 
 from utils import recursive_todevice
 from models.model_init import get_model, get_predict_loader
 from configure import get_config
+from data_prep import FEATURES
+
+cmap = ListedColormap(['grey', 'blue', 'purple', 'pink', 'green'])
+
+
+def unnormalize(x, config):
+    """ get original image data"""
+    x = x.squeeze()
+    x = x.reshape(x.shape[0] * x.shape[1], x.shape[2], x.shape[3])
+    x = x.permute(1, 2, 0)
+    mean_std = torch.tensor(pkl.load(open(config['dataset_folder'] + '/meanstd.pkl', 'rb')))
+    mean, std = mean_std[0], mean_std[1]
+    mean = mean.reshape(mean.shape[0] * mean.shape[1])
+    std = std.reshape(std.shape[0] * std.shape[1])
+    x = x.mul_(std).add_(mean)
+    x = x.detach().numpy()
+    return x
 
 
 def predict(config):
@@ -19,13 +40,8 @@ def predict(config):
     model.to(device)
     optimizer.load_state_dict(check_pt['optimizer'])
     model.eval()
-    prediction(model, val_loader, device, config)
-
-
-def prediction(model, loader, device, config):
-    print('writing predictions to {}'.format(config['res_dir']))
-    for i, (x, y) in enumerate(loader):
-        print(x.shape)
+    for i, (x, y) in enumerate(val_loader):
+        image = unnormalize(x, config)
         x = recursive_todevice(x, device)
         if config['model'] == 'clstm':
             y = y.argmax(dim=1).to(device)
@@ -35,32 +51,53 @@ def prediction(model, loader, device, config):
                 pred = torch.argmax(pred, dim=1)
         else:
             y = y.to(device)
+            y = y.argmax(dim=1)
             x = x.squeeze()
+            x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
+            x = x.permute(2, 0, 1)
             with torch.no_grad():
                 pred, att = model(x)
                 pred = torch.argmax(pred, dim=1)
+                pred = pred.reshape(y.shape)
 
-        plot_prediction(pred.cpu().numpy(), y.cpu().numpy(),
-                        os.path.join(config['res_dir'], 'figures', '{}.png'.format(i)))
-
-
-cmap = ListedColormap(['grey', 'blue', 'purple', 'pink', 'green'])
+        plot_prediction(image, pred.cpu().numpy(), y.cpu().numpy(), None)
+        # os.path.join(config['res_dir'], 'figures', '{}.png'.format(i)))
 
 
-def plot_prediction(pred, label, out_file=None):
-    fig, ax = plt.subplots(ncols=2, nrows=8, figsize=(10, 40))
-    for i in range(8):
-        ax[i, 0].imshow(pred[i], cmap=cmap)
-        ax[i, 1].imshow(label[i], cmap=cmap)
+def plot_prediction(x, pred, label, out_file=None):
+    cmap = ListedColormap(['grey', 'blue', 'purple', 'pink', 'green'])
+    r_idx, g_idx, b_idx = [FEATURES.index(x) for x in FEATURES if 'red' in x], \
+                          [FEATURES.index(x) for x in FEATURES if 'green' in x], \
+                          [FEATURES.index(x) for x in FEATURES if 'blue' in x]
+
+    fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(20, 10))
+    print(x.shape)
+    r, g, b = x[:, :, r_idx], x[:, :, g_idx], x[:, :, b_idx]
+
+    def norm_rgb(arr):
+        arr = (arr * 255).astype('uint8')
+        return arr
+
+    rgb = map(norm_rgb, [np.median(r, axis=2), np.median(g, axis=2), np.median(b, axis=2)])
+    rgb = np.dstack(rgb)
+
+    lat, lon = x[:, :, -3].mean(), x[:, :, -2].mean()
+
+    pred, label = pred.squeeze(), label.squeeze()
+    ax[0].imshow(rgb)
+    # ax[1].imshow(label, cmap=cmap)
+    # ax[2].imshow(pred, cmap=cmap)
+    plt.suptitle('{:.3f}, {:.3f}'.format(lat, lon))
     plt.tight_layout()
     if out_file:
         plt.savefig(out_file)
         plt.close()
     else:
         plt.show()
+        exit()
 
 
 if __name__ == '__main__':
-    config = get_config()
+    config = get_config('tcnn')
     predict(config)
 # ========================= EOF ====================================================================
