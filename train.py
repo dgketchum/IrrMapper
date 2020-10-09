4aa2887b9d4566facd0027076f3dfa5be029e8a9
dgketchum <pdWs7kyy>
2020-10-09 09:35:04 -0600

    refactor predictions and config into seperate module

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import os
 import json
 import pickle as pkl
-from pathlib import Path
 
 import torch
 import numpy as np
@@ -10,11 +9,9 @@ from torch.nn.modules.loss import CrossEntropyLoss
 from learning.focal_loss import FocalLoss
 from learning.weight_init import weight_init
 from learning.metrics import confusion_matrix_analysis
-from utils import plot_prediction
-
 from models.model_init import get_loaders, get_model
-
-path = Path(__file__).parents
+from configure import get_config
+from utils import recursive_todevice
 
 
 def train_epoch(model, optimizer, criterion, loader, device, config):
@@ -67,19 +64,6 @@ def evaluate_epoch(model, criterion, loader, device, config):
           'F1 Score {:.2f},'.format(i + 1, loss.item(), prec, rec, f1))
 
 
-def prediction(model, loader, device, config):
-    print('writing predictions to {}'.format(config['res_dir']))
-    for i, (x, y) in enumerate(loader):
-        x = recursive_todevice(x, device)
-        y = y.argmax(dim=1).to(device)
-        with torch.no_grad():
-            out, att = model(x)
-            pred = out[0][0]
-            pred = torch.argmax(pred, dim=1)
-            plot_prediction(pred.cpu().numpy(), y.cpu().numpy(),
-                            os.path.join(config['res_dir'], 'figures', '{}.png'.format(i)))
-
-
 def conf_matrix(y, pred, config, mask=None):
     confusion = torch.ones_like(config['confusion'])
     n_classes = config['num_classes']
@@ -97,13 +81,6 @@ def conf_matrix(y, pred, config, mask=None):
                 confusion[c, nc] = (pred == nc).bool().sum()
     config['confusion'] += confusion
     return confusion
-
-
-def recursive_todevice(x, device):
-    if isinstance(x, torch.Tensor):
-        return x.to(device)
-    else:
-        return [recursive_todevice(c, device) for c in x]
 
 
 def prepare_output(config):
@@ -134,20 +111,6 @@ def overall_performance(config):
 
     with open(os.path.join(config['res_dir'], 'overall.json'), 'w') as file:
         file.write(json.dumps(perf, indent=4))
-
-
-def predict(config):
-    device = torch.device(config['device'])
-    _, _, val_loader = get_loaders(config)
-    print('Val {}'.format(len(val_loader)))
-    model = get_model(config)
-    check_pt = torch.load(os.path.join(config['res_dir'], 'model.pth.tar'))
-    optimizer = torch.optim.Adam(model.parameters())
-    model.load_state_dict(check_pt['state_dict'])
-    model.to(device)
-    optimizer.load_state_dict(check_pt['optimizer'])
-    model.eval()
-    prediction(model, val_loader, device, config)
 
 
 def train(config):
@@ -201,92 +164,7 @@ def train(config):
 
 
 if __name__ == '__main__':
-
-    data = '/home/dgketchum/IrrigationGIS/tfrecords/tarchives'
-
-    if not os.path.isdir(data):
-        data = '/mnt/beegfs/dk128872/ts_data/cmask/tar'
-
-    config = {'mode': 'irr',
-              'rdm_seed': 1,
-              'epochs': 100,
-              'display_step': 10,
-              'num_classes': 4,
-              'nomenclature': 'label_4class',
-              'kfold': 5,
-              'input_dim': 7,
-              'geomfeat': None,
-              'device': 'cuda:0',
-              'num_workers': 1,
-              'pooling': 'mean_std',
-              'dropout': 0.2,
-              'gamma': 1,
-              'alpha': None,
-              'model': 'tcnn'}
-
-    if config['model'] == 'ltae':
-        config['dataset_folder'] = os.path.join(data, 'pixel_sets')
-        config['batch_size'] = 128
-        config['mlp1'] = '[7, 32, 64]'
-        config['mlp2'] = '[131, 128]'
-        config['mlp3'] = '[256,128]'
-        config['mlp4'] = '[128, 64, 32, 20]'
-        config['n_head'] = 16
-        config['d_k'] = 8
-        config['d_model'] = 256
-        config['T'] = 1000
-        config['positions'] = None
-        config['geom_dim'] = 3
-        config['geomfeat'] = True
-        config['lms'] = 13
-        config['npixel'] = 64
-        config['subset'] = None
-        config['lr'] = 0.00025
-        config['num_classes'] = 4
-        config['res_dir'] = os.path.join(path[0], 'models', 'ltae_pse', 'results')
-        with open(os.path.join(path[0], 'models', 'ltae_pse', 'config.json'), 'w') as file:
-            file.write(json.dumps(config, indent=4))
-            # exit()
-
-    if config['model'] == 'dcm':
-        config['batch_size'] = 1
-        config['dataset_folder'] = os.path.join(data, 'pixels')
-        config['hidden_size'] = 256
-        config['num_layers'] = 2
-        config['bidirectional'] = True
-        config['seed'] = 121
-        config['lr'] = 0.00025
-        config['res_dir'] = os.path.join(path[0], 'models', 'dcm', 'results')
-        with open(os.path.join(path[0], 'models', 'dcm', 'config.json'), 'w') as file:
-            file.write(json.dumps(config, indent=4))
-
-    if config['model'] == 'tcnn':
-        config['batch_size'] = 1
-        config['dataset_folder'] = os.path.join(data, 'pixels')
-        config['sequence_len'] = 13
-        config['nker'] = '[16, 16, 16]'
-        config['mlp3'] = '[16, 16]'
-        config['res_dir'] = os.path.join(path[0], 'models', 'temp_cnn', 'results')
-        with open(os.path.join(path[0], 'models', 'temp_cnn', 'config.json'), 'w') as file:
-            file.write(json.dumps(config, indent=4))
-
-    if config['model'] == 'clstm':
-        config['batch_size'] = 8
-        config['input_dim'] = 7
-        config['num_layers'] = 1
-        config['dataset_folder'] = os.path.join(data, 'images')
-        config['kernel_size'] = (3, 3)
-        config['hidden_dim'] = 4
-        config['res_dir'] = os.path.join(path[0], 'models', 'conv_lstm', 'results')
-        with open(os.path.join(path[0], 'models', 'conv_lstm', 'config.json'), 'w') as file:
-            file.write(json.dumps(config, indent=4))
-
-    for k, v in config.items():
-        if 'mlp' in k or k == 'nker':
-            v = v.replace('[', '')
-            v = v.replace(']', '')
-            config[k] = list(map(int, v.split(',')))
-
-    # pprint.pprint(config)
+    config = get_config()
     train(config)
-    # predict(config)
+
+# ========================================================================================
