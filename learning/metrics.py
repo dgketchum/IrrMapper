49604020ead06a312fb908d6a9896d12694532bd
dgketchum <pdWs7kyy>
2020-10-10 19:11:58 -0600

    conf mat broken

--- learning/metrics.py
+++ learning/metrics.py
@@ -1,54 +1,21 @@
-"""
-Credits to  https://github.com/VSainteuf/pytorch-psetae
-"""
 import numpy as np
 import torch
-import pandas as pd
 
 
-def mIou(y_true, y_pred, n_classes):
-    """
-    Mean Intersect over Union metric.
-    Computes the one versus all IoU for each class and returns the average.
-    Classes that do not appear in the provided set are not counted in the average.
-    Args:
-        y_true (1D-array): True labels
-        y_pred (1D-array): Predicted labels
-        n_classes (int): Total number of classes
-    Returns:
-        mean Iou (float)
-    """
-    iou = 0
-    n_observed = n_classes
-    for i in range(n_classes):
-        y_t = (np.array(y_true) == i).astype(int)
-        y_p = (np.array(y_pred) == i).astype(int)
-
-        inter = np.sum(y_t * y_p)
-        union = np.sum((y_t + y_p > 0).astype(int))
-
-        if union == 0:
-            n_observed -= 1
-        else:
-            iou += inter / union
-
-    return iou / n_observed
-
+def get_conf_matrix(y, pred, n_class, device):
+    batch_conf = torch.tensor(np.zeros((n_class, n_class))).to(device)
+    classes = torch.tensor([x for x in range(n_class)]).to(device)
+    for i in range(n_class):
+        c = classes[i]
+        pred, target = pred == c, y == c
+        batch_conf[i, i] = (pred & target).bool().sum()
+        for nc in range(n_class):
+            if nc != c:
+                batch_conf[c, nc] = (pred == nc).bool().sum()
+        return batch_conf
 
 
 def confusion_matrix_analysis(mat):
-    """
-    This method computes all the performance metrics from the confusion matrix. In addition to overall accuracy, the
-    precision, recall, f-score and IoU for each class is computed.
-    The class-wise metrics are averaged to provide overall indicators in two ways (MICRO and MACRO average)
-    Args:
-        mat (array): confusion matrix
-
-    Returns:
-        per_class (dict) : per class metrics
-        overall (dict): overall metrics
-
-    """
     TP = 0
     FP = 0
     FN = 0
@@ -71,19 +38,10 @@ def confusion_matrix_analysis(mat):
         TP += tp
         FP += fp
         FN += fn
-
-    overall = {}
-    overall['micro_IoU'] = TP / (TP + FP + FN)
-    overall['micro_Precision'] = TP / (TP + FP)
-    overall['micro_Recall'] = TP / (TP + FN)
-    overall['micro_F1-score'] = 2 * TP / (2 * TP + FP + FN)
-
-    # macro = pd.DataFrame(per_class).transpose().mean()
-    # overall['MACRO_IoU'] = macro.loc['IoU']
-    # overall['MACRO_Precision'] = macro.loc['Precision']
-    # overall['MACRO_Recall'] = macro.loc['Recall']
-    # overall['MACRO_F1-score'] = macro.loc['F1-score']
-
-    overall['Accuracy'] = torch.sum(torch.diag(mat)) / torch.sum(mat)
+    overall = {'IoU': TP / (TP + FP + FN),
+               'precision': TP / (TP + FP),
+               'recall': TP / (TP + FN),
+               'f1-score': 2 * TP / (2 * TP + FP + FN),
+               'accuracy': torch.sum(torch.diag(mat)) / torch.sum(mat)}
 
     return per_class, overall
