1cc2979dea5efe743113933d6c31386e190b604a
dgketchum <pdWs7kyy>
2020-10-15 20:17:05 -0600

    seperate image dataset and predict dataset

--- data_prep/dataset.py
+++ data_prep/dataset.py
@@ -1,16 +1,9 @@
 import os
-import numpy as np
 import torch
 import webdataset as wds
 from webdataset import dataset as wds
 
 from data_prep import BANDS, CHANNELS, TERRAIN, SEQUENCE_LEN
-
-structure = np.array([
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 1]
-])
 
 
 def transform_(x, mean_std):
@@ -69,33 +62,43 @@ def pixelset_dataset(mode, config, norm):
     return ds
 
 
-def image_dataset(mode, config, norm, pixel_predict=False):
-
+def image_dataset(mode, config, norm):
+    """ Use for training and prediction of image datasets"""
     def map_fn(item):
         features = item['pth'][:, :, :BANDS + TERRAIN]
         features = transform_(features, norm).float()
         x = features[:, :, :BANDS]
         x = x.permute(2, 0, 1)
-
-        if pixel_predict:
-            x = x.permute(1, 2, 0)
-            x = x.reshape(x.shape[0], x.shape[1], SEQUENCE_LEN, CHANNELS).float()
-            y = item['pth'][:, :, -4:].permute(2, 0, 1)
-        else:
-            x = x.reshape(x.shape[1], x.shape[2], SEQUENCE_LEN, CHANNELS)
-            x = x.permute((2, 3, 0, 1)).float()
-            y = item['pth'][:, :, -4:].permute(2, 0, 1).int()
+        x = x.reshape(x.shape[1], x.shape[2], SEQUENCE_LEN, CHANNELS)
+        x = x.permute((2, 3, 0, 1)).float()
+        y = item['pth'][:, :, -4:].permute(2, 0, 1).int()
+        print('image', x.shape)
 
         g = features[:, :, BANDS:BANDS + TERRAIN].permute(2, 0, 1)
         return x, y, g
 
-    if pixel_predict:
-        data_dir = config['prediction_dir']
-        loc = os.path.join(data_dir, '{}_patches'.format(mode))
-    else:
-        data_dir = config['dataset_folder']
-        loc = os.path.join(data_dir, mode, '{}_patches'.format(mode))
+    data_dir = config['dataset_folder']
+    loc = os.path.join(data_dir, mode, '{}_patches'.format(mode))
+    end_idx = len(os.listdir(loc)) - 1
+    brace_str = '{}_{{000000..{}}}.tar'.format(mode, str(end_idx).rjust(6, '0'))
+    url = os.path.join(loc, brace_str)
+    dataset = wds.Dataset(url).decode('torchl').map(map_fn).batched(config['batch_size'])
+    return dataset
 
+
+def predict_dataset(mode, config, norm):
+    """ Use for prediction of images using pixel-based models """
+    def map_fn(item):
+        features = item['pth'][:, :, :BANDS + TERRAIN]
+        features = transform_(features, norm).float()
+        x = features[:, :, :BANDS]
+        x = x.reshape(x.shape[0], x.shape[1], SEQUENCE_LEN, CHANNELS).float()
+        y = item['pth'][:, :, -4:].permute(2, 0, 1)
+        g = features[:, :, BANDS:BANDS + TERRAIN].permute(2, 0, 1)
+        return x, y, g
+
+    data_dir = config['prediction_dir']
+    loc = os.path.join(data_dir, '{}_patches'.format(mode))
     end_idx = len(os.listdir(loc)) - 1
     brace_str = '{}_{{000000..{}}}.tar'.format(mode, str(end_idx).rjust(6, '0'))
     url = os.path.join(loc, brace_str)
