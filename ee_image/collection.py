ea4c0cd00e1cdebf3112665c0ac2cce9764aab26
dgketchum <pdWs7kyy>
2021-02-04 13:25:22 -0700

    initial commit openet-based image package

---
+++ ee_image/collection.py
@@ -0,0 +1,232 @@
+import os
+from pprint import pprint
+from datetime import datetime, timedelta
+from dateutil.rrule import rrule, DAILY
+import ee
+
+from ee_image.image import Image
+import ee_image.inerpolate as interp
+
+
+def lazy_property(fn):
+    """Decorator that makes a property lazy-evaluated
+
+    https://stevenloria.com/lazy-properties/
+    """
+    attr_name = '_lazy_' + fn.__name__
+
+    @property
+    def _lazy_property(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return _lazy_property
+
+
+class Collection:
+    """"""
+
+    def __init__(
+            self,
+            collections,
+            start_date,
+            end_date,
+            geometry,
+            variables=None,
+            mask=True,
+            cloud_cover_max=70):
+
+        self.collections = collections
+        self.mask = mask
+        self.start_date = start_date
+        self.end_date = end_date
+        self.start_str = self.start_date.strftime('%Y-%m-%d')
+        self.end_str = self.end_date.strftime('%Y-%m-%d')
+
+        self.variables = variables
+        self.geometry = geometry
+        self.cloud_cover_max = cloud_cover_max
+        self._interp_vars = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'tir']
+
+        # If collections is a string, place in a list
+        if type(self.collections) is str:
+            self.collections = [self.collections]
+
+    def _build(self, interp_vars):
+        """Build a merged model variable image collection
+
+        Parameters
+        ----------
+        variables : list
+            Set a variable list that is different than the class variable list.
+        start_date : str, optional
+            Set a start_date that is different than the class start_date.
+            This is needed when defining the scene collection to have extra
+            images for interpolation.
+        end_date : str, optional
+            Set an exclusive end_date that is different than the class end_date.
+
+        Returns
+        -------
+        ee.ImageCollection
+
+        Raises
+        ------
+        ValueError if collection IDs are invalid.
+        ValueError if variables is not set here and in class init.
+
+        """
+
+        # Build the variable image collection
+        variable_coll = ee.ImageCollection([])
+        for coll_id in self.collections:
+            input_coll = ee.ImageCollection(coll_id) \
+                .filterDate(self.start_date, self.end_date) \
+                .filterBounds(self.geometry) \
+                .filterMetadata('CLOUD_COVER_LAND', 'less_than',
+                                self.cloud_cover_max)
+
+            # Time filters are to remove bad (L5) and pre-op (L8) images
+            if 'LT05' in coll_id:
+                input_coll = input_coll.filter(ee.Filter.lt(
+                    'system:time_start', ee.Date('2011-12-31').millis()))
+            elif 'LC08' in coll_id:
+                input_coll = input_coll.filter(ee.Filter.gt(
+                    'system:time_start', ee.Date('2013-03-24').millis()))
+
+            def compute_lsr(image):
+                model_obj = Image.from_landsat_c1_sr(
+                    sr_image=ee.Image(image), mask=self.mask)
+                return model_obj.calculate(interp_vars)
+
+            variable_coll = variable_coll.merge(
+                ee.ImageCollection(input_coll.map(compute_lsr)))
+
+        return variable_coll
+
+    def scenes(self, variables):
+        interp_vars = [band for band in variables]
+        interp_vars.append('time')
+        scene_coll = self._build(interp_vars)
+        return ee.ImageCollection(scene_coll).select(variables)
+
+    def interpolate(self, variables, interp_days=32, dates=None):
+
+        ref_et = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET') \
+            .filter(ee.Filter.inList("system:time_start", dates)) \
+            .select(['etr'], ['et_reference'])
+
+        interp_vars = [band for band in variables]
+
+        # Count will be determined using the aggregate_coll image masks
+        if 'count' in variables:
+            interp_vars.append('mask')
+
+        interp_vars.append('time')
+
+        # Build initial scene image collection
+        scene_coll = self._build(interp_vars)
+        # For count, compute the composite/mosaic image for the mask band only
+        if 'count' in variables:
+            aggregate_coll = interp.aggregate_to_daily(
+                image_coll=scene_coll.select(['mask']),
+                start_date=self.start_date, end_date=self.end_date)
+
+            aggregate_coll = aggregate_coll.merge(
+                ee.Image.constant(0).rename(['mask']).set({'system:time_start': ee.Date(self.start_str).millis()}))
+
+        if 'mask' in interp_vars:
+            interp_vars.remove('mask')
+
+        # Interpolate to a daily time step
+        daily_coll = interp.daily(
+            target_coll=ref_et,
+            source_coll=scene_coll.select(interp_vars),
+            interp_days=interp_days,
+            use_joins=True)
+
+        interp_properties = {
+            'cloud_cover_max': self.cloud_cover_max,
+            'collections': ', '.join(self.collections),
+            'interp_days': interp_days,
+            'interp_method': 'linear',
+            'model_name': 'IrrMapper'}
+
+        def aggregate_image(agg_start_date, agg_end_date, date_format):
+
+            image_list = []
+
+            for var in variables:
+                # Compute average of variable over the aggregation period
+                img = daily_coll \
+                    .filterDate(agg_start_date, agg_end_date) \
+                    .mean().select([var]).float()
+
+                image_list.append(img)
+
+            if 'count' in variables:
+                count_img = aggregate_coll \
+                    .filterDate(agg_start_date, agg_end_date) \
+                    .select(['mask']).count().rename('count').uint8()
+                image_list.append(count_img)
+
+            agg_image = ee.Image(image_list) \
+                .set(interp_properties) \
+                .set({'system:index': ee.Date(agg_start_date).format(date_format),
+                      'system:time_start': ee.Date(agg_start_date).millis(),
+                      })
+
+            return agg_image
+
+        def aggregate_daily(daily_img):
+            agg_start_date = ee.Date(daily_img.get('system:time_start'))
+            return aggregate_image(
+                agg_start_date=agg_start_date,
+                agg_end_date=ee.Date(agg_start_date).advance(1, 'day'),
+                date_format='YYYYMMdd')
+
+        interp_collection = ee.ImageCollection(daily_coll.map(aggregate_daily)).select(variables)
+        return interp_collection
+
+    def get_image_ids(self):
+        """Return image IDs of the input images
+
+        Returns
+        -------
+        list
+
+        Notes
+        -----
+        This image list is based on the collection start and end dates and may
+        not include all of the images used for interpolation.
+
+        """
+        return sorted(list(self._build().aggregate_array('image_id').getInfo()))
+
+
+def get_target_dates(s, e, interval_=15):
+    d_times = [(d, d + timedelta(days=1)) for d in rrule(dtstart=s, until=e, interval=interval_, freq=DAILY)]
+    d_strings = [(x.strftime('%Y-%m-%d'), y.strftime('%Y-%m-%d')) for x, y in d_times]
+    gm = ee.ImageCollection('IDAHO_EPSCOR/GRIDMET')
+    images = [gm.filterDate(s, e).first().getInfo()['properties']['system:time_start'] for s, e in d_strings]
+    return images
+
+
+def get_target_bands(s, e, interval_=15, vars=None):
+    d_times = [d for d in rrule(dtstart=s, until=e, interval=interval_, freq=DAILY)]
+    d_strings = [x.strftime('%Y%m%d') for x in d_times]
+    ints_ = [x for x in range(len(d_strings))]
+
+    collection_bands = [['{}_{}'.format(d, b) for b in vars] for d in d_strings]
+    collection_bands = [item for sublist in collection_bands for item in sublist]
+
+    rename_bands = [['{}_{}'.format(b, d) for b in vars] for d in ints_]
+    rename_bands = [item for sublist in rename_bands for item in sublist]
+
+    return collection_bands, rename_bands
+
+
+if __name__ == '__main__':
+    pass
+# =======================================================================================
---
+++ ee_image/inerpolate.py
@@ -0,0 +1,287 @@
+
+import datetime
+import logging
+
+import ee
+from dateutil.relativedelta import *
+from ee_image import utils
+
+
+def daily(target_coll, source_coll, interp_days=32,
+          use_joins=False, compute_product=False):
+    """Interpolate non-daily source images to a daily target image collection
+    Parameters
+    ----------
+    target_coll : ee.ImageCollection
+        Source images will be interpolated to each target image time_start.
+        Target images should have a daily time step.  This will typically be
+        the reference ET (ETr) collection.
+    source_coll : ee.ImageCollection
+        Images that will be interpolated to the target image collection.
+        This will typically be the fraction of reference ET (ETrF) collection.
+    interp_days : int, optional
+        Number of days before and after each image date to include in the
+        interpolation (the default is 32).
+    use_joins : bool, optional
+        If True, the source collection will be joined to the target collection
+        before mapping/interpolation and the source images will be extracted
+        from the join properties ('prev' and 'next').
+        Setting use_joins=True should be more memory efficient.
+        If False, the source images will be built by filtering the source
+        collection separately for each image in the target collection
+        (inside the mapped function).
+    compute_product : bool, optional
+        If True, compute the product of the target and all source image bands.
+        The default is False.
+    Returns
+    -------
+    ee.ImageCollection() of daily interpolated images
+    Raises
+    ------
+    ValueError
+        If `interp_method` is not a supported method.
+    """
+
+    prev_filter = ee.Filter.And(
+        ee.Filter.maxDifference(
+            difference=(interp_days + 1) * 24 * 60 * 60 * 1000,
+            leftField='system:time_start',
+            rightField='system:time_start',
+        ),
+        ee.Filter.greaterThan(
+            leftField='system:time_start',
+            rightField='system:time_start',
+        )
+    )
+
+    next_filter = ee.Filter.And(
+        ee.Filter.maxDifference(
+            difference=(interp_days + 1) * 24 * 60 * 60 * 1000,
+            leftField='system:time_start',
+            rightField='system:time_start',
+        ),
+        ee.Filter.lessThanOrEquals(
+            leftField='system:time_start',
+            rightField='system:time_start',
+        )
+    )
+
+    if use_joins:
+        # Join the neighboring Landsat images in time
+        target_coll = ee.ImageCollection(
+            ee.Join.saveAll(
+                matchesKey='prev',
+                ordering='system:time_start',
+                ascending=True,
+                outer=True,
+            ).apply(
+                primary=target_coll,
+                secondary=source_coll,
+                condition=prev_filter,
+            )
+        )
+
+        target_coll = ee.ImageCollection(
+            ee.Join.saveAll(
+                matchesKey='next',
+                ordering='system:time_start',
+                ascending=False,
+                outer=True,
+            ).apply(
+                primary=target_coll,
+                secondary=source_coll,
+                condition=next_filter,
+            )
+        )
+
+    def _linear(image):
+        """Linearly interpolate source images to target image time_start(s)
+        Parameters
+        ----------
+        image : ee.Image.
+            The first band in the image will be used as the "target" image
+            and will be returned with the output image.
+        Returns
+        -------
+        ee.Image of interpolated values with band name 'src'
+        Notes
+        -----
+        The source collection images must have a time band.
+        This function is intended to be mapped over an image collection and
+            can only take one input parameter.
+        """
+        # target_img = ee.Image(image).select(0).double()
+        target_date = ee.Date(image.get('system:time_start'))
+
+        # All filtering will be done based on 0 UTC dates
+        utc0_date = utils.date_0utc(target_date)
+        # utc0_time = target_date.update(hour=0, minute=0, second=0)\
+        #     .millis().divide(1000).floor().multiply(1000)
+        time_img = ee.Image.constant(utc0_date.millis()).double()
+
+        # Build nodata images/masks that can be placed at the front/back of
+        #   of the qm image collections in case the collections are empty.
+        bands = source_coll.first().bandNames()
+        prev_qm_mask = ee.Image.constant(ee.List.repeat(1, bands.length())) \
+            .double().rename(bands).updateMask(0) \
+            .set({
+            'system:time_start': utc0_date.advance(
+                -interp_days - 1, 'day').millis()})
+        next_qm_mask = ee.Image.constant(ee.List.repeat(1, bands.length())) \
+            .double().rename(bands).updateMask(0) \
+            .set({
+            'system:time_start': utc0_date.advance(
+                interp_days + 2, 'day').millis()})
+
+        if use_joins:
+            # Build separate mosaics for before and after the target date
+            prev_qm_img = ee.ImageCollection \
+                .fromImages(ee.List(ee.Image(image).get('prev'))) \
+                .merge(ee.ImageCollection(prev_qm_mask)) \
+                .sort('system:time_start', True) \
+                .mosaic()
+            next_qm_img = ee.ImageCollection \
+                .fromImages(ee.List(ee.Image(image).get('next'))) \
+                .merge(ee.ImageCollection(next_qm_mask)) \
+                .sort('system:time_start', False) \
+                .mosaic()
+        else:
+            # Build separate collections for before and after the target date
+            prev_qm_coll = source_coll \
+                .filterDate(utc0_date.advance(-interp_days, 'day'), utc0_date) \
+                .merge(ee.ImageCollection(prev_qm_mask))
+            next_qm_coll = source_coll \
+                .filterDate(utc0_date, utc0_date.advance(interp_days + 1, 'day')) \
+                .merge(ee.ImageCollection(next_qm_mask))
+
+            # Flatten the previous/next collections to single images
+            # The closest image in time should be on "top"
+            # CGM - Is the previous collection already sorted?
+            # prev_qm_img = prev_qm_coll.mosaic()
+            prev_qm_img = prev_qm_coll.sort('system:time_start', True) \
+                .mosaic()
+            next_qm_img = next_qm_coll.sort('system:time_start', False) \
+                .mosaic()
+
+        # DEADBEEF - It might be easier to interpolate all bands instead of
+        #   separating the value and time bands
+        # prev_value_img = ee.Image(prev_qm_img).double()
+        # next_value_img = ee.Image(next_qm_img).double()
+
+        # Interpolate all bands except the "time" band
+        prev_bands = prev_qm_img.bandNames() \
+            .filter(ee.Filter.notEquals('item', 'time'))
+        next_bands = next_qm_img.bandNames() \
+            .filter(ee.Filter.notEquals('item', 'time'))
+        prev_value_img = ee.Image(prev_qm_img.select(prev_bands)).double()
+        next_value_img = ee.Image(next_qm_img.select(next_bands)).double()
+        prev_time_img = ee.Image(prev_qm_img.select('time')).double()
+        next_time_img = ee.Image(next_qm_img.select('time')).double()
+
+        # Fill masked values with values from the opposite image
+        # Something like this is needed to ensure there are always two
+        #   values to interpolate between
+        # For data gaps, this will cause a flat line instead of a ramp
+        prev_time_mosaic = ee.Image(ee.ImageCollection.fromImages([
+            next_time_img, prev_time_img]).mosaic())
+        next_time_mosaic = ee.Image(ee.ImageCollection.fromImages([
+            prev_time_img, next_time_img]).mosaic())
+        prev_value_mosaic = ee.Image(ee.ImageCollection.fromImages([
+            next_value_img, prev_value_img]).mosaic())
+        next_value_mosaic = ee.Image(ee.ImageCollection.fromImages([
+            prev_value_img, next_value_img]).mosaic())
+
+        # Calculate time ratio of the current image between other cloud free images
+        time_ratio_img = time_img.subtract(prev_time_mosaic) \
+            .divide(next_time_mosaic.subtract(prev_time_mosaic))
+
+        # Interpolate values to the current image time
+        interp_img = next_value_mosaic.subtract(prev_value_mosaic) \
+            .multiply(time_ratio_img).add(prev_value_mosaic)
+
+        # Pass the target image back out as a new band
+        target_img = image.select([0]).double()
+        output_img = interp_img.addBands([target_img])
+
+        if compute_product:
+            output_img = output_img \
+                .addBands([interp_img.multiply(target_img)])
+
+        return output_img.set({
+            'system:index': image.get('system:index'),
+            'system:time_start': image.get('system:time_start'),
+            # 'system:time_start': utc0_time,
+        })
+
+    interp_coll = ee.ImageCollection(target_coll.map(_linear))
+
+    return interp_coll
+
+
+def aggregate_to_daily(image_coll, start_date=None, end_date=None,
+                       agg_type='mean'):
+    """Aggregate images by day without using joins
+    The primary purpose of this function is to join separate Landsat images
+    from the same path into a single daily image.
+    Parameters
+    ----------
+    image_coll : ee.ImageCollection
+        Input image collection.
+    start_date :  date, number, string, optional
+        Start date.
+        Needs to be an EE readable date (i.e. ISO Date string or milliseconds).
+    end_date :  date, number, string, optional
+        Exclusive end date.
+        Needs to be an EE readable date (i.e. ISO Date string or milliseconds).
+    agg_type : {'mean'}, optional
+        Aggregation type (the default is 'mean').
+        Currently only a 'mean' aggregation type is supported.
+    Returns
+    -------
+    ee.ImageCollection()
+    Notes
+    -----
+    This function should be used to mosaic Landsat images from same path
+        but different rows.
+    system:time_start of returned images will be 0 UTC (not the image time).
+    """
+    if start_date and end_date:
+        test_coll = image_coll.filterDate(ee.Date(start_date), ee.Date(end_date))
+    elif start_date:
+        test_coll = image_coll.filter(ee.Filter.greaterThanOrEquals(
+            'system:time_start', ee.Date(start_date).millis()))
+    elif end_date:
+        test_coll = image_coll.filter(ee.Filter.lessThan(
+            'system:time_start', ee.Date(end_date).millis()))
+    else:
+        test_coll = image_coll
+
+    # Build a sorted list of the unique "dates" in the image_coll
+    date_list = ee.List(test_coll.aggregate_array('system:time_start')) \
+        .map(lambda time: ee.Date(ee.Number(time)).format('yyyy-MM-dd')) \
+        .distinct().sort()
+
+    def aggregate_func(date_str):
+        start_date = ee.Date(ee.String(date_str))
+        end_date = start_date.advance(1, 'day')
+        agg_coll = image_coll.filterDate(start_date, end_date)
+
+        if agg_type.lower() == 'mean':
+            agg_img = agg_coll.mean()
+        # elif agg_type.lower() == 'median':
+        #     agg_img = agg_coll.median()
+        else:
+            raise ValueError(f'unsupported agg_type "{agg_type}"')
+
+        return agg_img.set({
+            'system:index': start_date.format('yyyyMMdd'),
+            'system:time_start': start_date.millis(),
+            'date': start_date.format('yyyy-MM-dd'),
+        })
+
+    return ee.ImageCollection(date_list.map(aggregate_func))
+
+
+if __name__ == '__main__':
+    pass
+# ========================= EOF ====================================================================
