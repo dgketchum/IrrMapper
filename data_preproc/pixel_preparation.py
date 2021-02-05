a183c60b496ddba78753be3617a525962dd9aa82
dgketchum <pdWs7kyy>
2021-02-05 13:48:28 -0700

    write tfrecords directly to class-balanced pixel sets

--- data_preproc/pixel_preparation.py
+++ data_preproc/pixel_preparation.py
@@ -1,5 +1,4 @@
 import os
-import scipy.ndimage.measurements as mnts
 import numpy as np
 import pickle as pkl
 import webdataset as wds
@@ -17,7 +16,6 @@ structure = np.array([
 
 
 def push_tar(t_dir, out_dir, mode, items, ind):
-
     tar_filename = '{}_{}.tar'.format(mode, str(ind).zfill(6))
     tar_archive = os.path.join(out_dir, tar_filename)
     with tarfile.open(tar_archive, 'w') as tar:
@@ -26,161 +24,116 @@ def push_tar(t_dir, out_dir, mode, items, ind):
     shutil.rmtree(t_dir)
 
 
-def write_pixel_sets(out, recs, mode, out_norm):
-    """Iterate through each image of feature bands and labels, find contiguous objects, extract
-    pixel set from within the objects
-    """
-    count = 0
+def write_pixel_blocks(data_dir, out, mode, n_subset=1000, out_norm=None):
+    """ write tensorflow records to class-balanced torch arrays every n samples"""
+
+    label_count, out_pixels = 0, 0
     invalid_pix, nan_pix = 0, 0
     mean_, std_ = 0, 0
     M2 = 0
-    obj_ct = {0: 0, 1: 0, 2: 0}
+    obj_cntr = {0: None, 1: None, 2: None}
     pix_ct = {0: 0, 1: 0, 2: 0}
 
-    url_list = [[os.path.join(recs, x) for x in os.listdir(recs) if x.endswith('.tar')]]
+    url_list = [[os.path.join(data_dir, x) for x in os.listdir(data_dir) if x.endswith('.tar')]]
 
     for urls in url_list:
+
         dataset = wds.Dataset(urls).decode('torchl')
-        tar_count, items = 0, []
+
+        items, tar_count, = [], 0
+
         tmpdirname = tempfile.mkdtemp()
+
         for j, f in enumerate(dataset):
             a = f['pth'].numpy()
-            # treat cdl and cdl_confidence [97, 98] as features
             labels = a[:, :, 98:]
             features = a[:, :, :98]
 
-            bbox_slices = {}
             for i in range(labels.shape[2]):
                 _class = i
                 lab = labels[:, :, i].copy()
                 if lab.max():
-                    bbox_slices[i] = mnts.find_objects(mnts.label(lab, structure=structure)[0])
-                    for b in bbox_slices[i]:
-                        # features
-                        lab_mask = np.repeat(lab[b][:, :, np.newaxis], features.shape[-1], axis=2)
-                        nan_label = lab_mask.copy()
-                        nan_label[:, :, :] = np.iinfo(np.uint32).min
-                        feat = np.where(lab_mask, features[b], nan_label)
-                        feat[feat == np.iinfo(np.uint32).min] = np.nan
+                    lab_mask = np.repeat(lab[:, :, np.newaxis], features.shape[-1], axis=2)
+                    nan_label = lab_mask.copy()
+                    nan_label[:, :, :] = np.iinfo(np.uint32).min
+                    feat = np.where(lab_mask, features, nan_label)
+                    feat[feat == np.iinfo(np.uint32).min] = np.nan
 
-                        feat = feat[:, :, :BANDS + TERRAIN]
-                        feat = feat.reshape(feat.shape[0] * feat.shape[1], BANDS + TERRAIN)
-                        nan_mask = np.any(np.isnan(feat), axis=1)
-                        feat = feat[~nan_mask]
+                    feat = feat[:, :, :BANDS + TERRAIN]
+                    feat = feat.reshape(feat.shape[0] * feat.shape[1], BANDS + TERRAIN)
+                    nan_mask = np.any(np.isnan(feat), axis=1)
+                    feat = feat[~nan_mask]
 
-                        if feat.shape[0] < 1:
-                            nan_pix += 1
-                            continue
+                    if feat.shape[0] < 1:
+                        nan_pix += 1
+                        continue
 
-                        if np.any(feat[:, 0] == 2.0):
-                            invalid_pix += 1
-                            continue
+                    if np.any(feat[:, 0] == 2.0):
+                        invalid_pix += 1
+                        continue
 
-                        count += 1
+                    label_count += 1
 
-                        label = np.ones((feat.shape[0], 1)) * _class
+                    label = np.ones((feat.shape[0], 1)) * _class
+                    pixel_stack = np.append(feat, label, axis=1)
+                    pix_ct[_class] += feat.shape[0]
 
-                        # update mean and std
-                        # mean_std.shape =  C x T
-                        delta = np.nanmean(feat, axis=0) - mean_
-                        mean_ = mean_ + delta / count
-                        delta2 = np.nanmean(feat, axis=0) - mean_
-                        M2 = M2 + delta * delta2
-                        std_ = np.sqrt(M2 / count)
+                    # update mean and std
+                    # mean_std.shape =  C x T
+                    delta = np.nanmean(feat, axis=0) - mean_
+                    mean_ = mean_ + delta / label_count
+                    delta2 = np.nanmean(feat, axis=0) - mean_
+                    M2 = M2 + delta * delta2
+                    std_ = np.sqrt(M2 / label_count)
 
-                        obj_ct[_class] += 1
-                        pix_ct[_class] += feat.shape[0]
+                    if isinstance(obj_cntr[_class], np.ndarray):
+                        obj_cntr[_class] = np.append(obj_cntr[_class], pixel_stack, axis=0)
+                    else:
+                        obj_cntr[_class] = pixel_stack
 
-                        if count % 1000 == 0:
-                            print('count: {}'.format(count))
+                    obj_state = np.array([isinstance(obj_cntr[x], type(None)) for x in obj_cntr.keys()])
+                    if obj_state.any():
+                        continue
 
-                        tmp_tensor = os.path.join(tmpdirname, '{}.pth'.format(str(count).rjust(7, '0')))
+                    counts = np.array([obj_cntr[x].shape[0] > n_subset for x in obj_cntr.keys()])
+                    if counts.all():
+                        indices = [np.array([x for x in range(0, obj_cntr[y].shape[0])]) for y in obj_cntr.keys()]
+                        choice = [np.random.choice(i, n_subset, replace=False) for i in indices]
+                        choice_inv = [[i for i in ind if i not in choice[x]] for x, ind in enumerate(indices)]
+                        select = [obj_cntr[x][c, :] for x, c in zip(obj_cntr.keys(), choice)]
+                        left_over = [obj_cntr[x][ac, :] for x, ac in zip(obj_cntr.keys(), choice_inv)]
 
-                        out_array = np.append(feat, label, axis=1)
-                        feat = torch.from_numpy(out_array)
-                        torch.save(feat, tmp_tensor)
-
+                        array = np.concatenate(select, axis=0)
+                        out_pixels += array.shape[0]
+                        array = torch.from_numpy(array)
+                        tmp_tensor = os.path.join(tmpdirname, '{}.pth'.format(str(label_count).rjust(7, '0')))
+                        label_count += 1
+                        torch.save(array, tmp_tensor)
                         items.append(tmp_tensor)
 
-                        if len(items) == 200:
-                            push_tar(tmpdirname, out, mode, items, tar_count)
-                            tmpdirname = tempfile.mkdtemp()
-                            items = []
-                            tar_count += 1
+                        for k, v in obj_cntr.items():
+                            if v.shape[0] > 5 * n_subset:
+                                left_over[k] = v[:n_subset * 5, :]
+                            obj_cntr[k] = left_over[k]
 
-        if len(items) > 0:
-            push_tar(tmpdirname, out, mode, items, tar_count)
+                    if len(items) == 200:
+                        push_tar(tmpdirname, out, mode, items, tar_count)
+                        tmpdirname = tempfile.mkdtemp()
+                        items = []
+                        tar_count += 1
 
-        print('{} objects count: {}'.format(split, obj_ct))
-        print('pixel count: {}'.format(pix_ct))
-        print('nan pixels: {}, 2.0 pixels: {}'.format(nan_pix, invalid_pix))
-        print('count of pixel sets: {}'.format(count))
+    if len(items) > 0:
+        push_tar(tmpdirname, out, mode, items, tar_count)
 
-        if out_norm:
-            pkl_name = os.path.join(out_norm, 'meanstd.pkl')
-            with open(pkl_name, 'wb') as handle:
-                pkl.dump((mean_, std_), handle, protocol=pkl.HIGHEST_PROTOCOL)
+    print('\n{}'.format(mode.upper()))
+    print('pixel count: {}, {} total'.format(pix_ct, out_pixels))
+    print('nan objects: {}, 2.0 objects: {}'.format(nan_pix, invalid_pix))
+    print('count of pixel sets: {}'.format(label_count))
 
-
-def write_pixel_blocks(data_dir, out, mode, n_subset=1000):
-    """ write numpy arrays every n samples from pixel sets"""
-
-    if mode == 'train' and 'points' in data_dir:
-        url_list = []
-        prefixes = ['d', 'i', 'f']
-        for _type in prefixes:
-            url_list.append([os.path.join(data_dir, x) for x in os.listdir(data_dir) if x.endswith('.tar') and _type in x])
-    else:
-        url_list = [[os.path.join(data_dir, x) for x in os.listdir(data_dir) if x.endswith('.tar')]]
-        prefixes = [None]
-
-    for urls, pref in zip(url_list, prefixes):
-
-        first = True
-        count, file_count = 0, 0
-        remainder = np.array(False)
-        tmpdirname = tempfile.mkdtemp()
-        items, tar_count, features = [], 0, None
-
-        dataset = wds.Dataset(urls).decode('torchl').to_tuple('pth')
-        for sample in dataset:
-            a = sample[0].numpy()
-            if first:
-                features = a
-                first = False
-                if remainder.any():
-                    features = np.append(features, remainder, axis=0)
-            else:
-                features = np.append(features, a, axis=0)
-            if features.shape[0] > n_subset:
-                remainder = features[n_subset:, :]
-                out_features = torch.tensor(features[:n_subset, :])
-                tmp_feat = os.path.join(tmpdirname, '{}.pth'.format(str(file_count).rjust(7, '0')))
-
-                torch.save(out_features, tmp_feat)
-
-                file_count += 1
-                features = None
-                count += 1
-                first = True
-                items.append(tmp_feat)
-
-                if len(items) == 4:
-                    push_tar(tmpdirname, out, mode, items, tar_count)
-                    tmpdirname = tempfile.mkdtemp()
-                    items = []
-                    tar_count += 1
-
-        if features is not None:
-            while features.shape[-1] < n_subset:
-                features = np.append(features, features, axis=-1)
-            file_count += 1
-            out_features = torch.tensor(features[:n_subset, :])
-            tmp_feat = os.path.join(tmpdirname, '{}.pth'.format(str(file_count).rjust(7, '0')))
-            torch.save(out_features, tmp_feat)
-            print('final file {}'.format(file_count))
-            items.append(tmp_feat)
-            push_tar(tmpdirname, out, mode, items, tar_count)
+    if out_norm:
+        pkl_name = os.path.join(out_norm, 'meanstd.pkl')
+        with open(pkl_name, 'wb') as handle:
+            pkl.dump((mean_, std_), handle, protocol=pkl.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
@@ -189,21 +142,15 @@ if __name__ == '__main__':
     if not os.path.isdir(data):
         data = '/mnt/beegfs/dk128872/ts_data/cmask/tar'
 
-    images = os.path.join(data, 'images')
-    pixels = os.path.join(data, 'pixels')
-    pixel_sets = os.path.join(data, 'pixel_sets')
+    images_dir = os.path.join(data, 'images')
+    pixels_dir = os.path.join(data, 'pixels')
 
     for split in ['valid', 'test', 'train']:
-        np_images = os.path.join(images, split)
-        pixel_dst = os.path.join(pixels, split)
-        pixel_set_dst = os.path.join(pixel_sets, split)
-
-        out_norm_pse = pixel_sets
+        np_images = os.path.join(images_dir, split)
+        pixel_dst = os.path.join(pixels_dir, split)
         if split == 'train':
-            write_pixel_sets(pixel_set_dst, np_images, split, out_norm=out_norm_pse)
+            write_pixel_blocks(data_dir=np_images, out=pixel_dst, mode=split, out_norm=pixels_dir)
         else:
-            write_pixel_sets(pixel_set_dst, np_images, split, out_norm=None)
-
-        write_pixel_blocks(data_dir=pixel_set_dst, out=pixel_dst, mode=split)
+            write_pixel_blocks(data_dir=np_images, out=pixel_dst, mode=split)
 
 # ========================= EOF ================================================================
