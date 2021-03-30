4c4ec07ece2ceb47df45685acc5f7dcd17db8794
dgketchum <pdWs7kyy>
2021-03-30 12:24:37 -0600

    set up for unet on nas

--- data_load/dataset.py
+++ data_load/dataset.py
@@ -21,6 +21,11 @@ class IrrMapDataset(Dataset):
         self.mode = mode
         self.model = model
         self.img_paths = glob(os.path.join(data_dir, '*.pth'))
+        # add test to train for two-way train/valid split
+        if 'train' in data_dir:
+            add_paths = glob(os.path.join(data_dir.replace('train', 'test'), '*.pth'))
+            self.img_paths += add_paths
+
         self.transforms = transforms
 
     def __getitem__(self, item):
