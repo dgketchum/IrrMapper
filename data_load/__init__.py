60a2a29e7e027fcd14ea68dd19395b6a1e23cd19
dgketchum <pdWs7kyy>
2021-04-16 11:19:02 -0600

    recal batch size and min epochs to 100

--- data_load/__init__.py
+++ data_load/__init__.py
@@ -1,8 +1,11 @@
 import os
 import pickle as pkl
 
-NORM = pkl.load(open('/media/nvm/ts_data/cm/pixels/meanstd.pkl', 'rb'))
-
+try:
+    NORM = pkl.load(open('/media/nvm/ts_data/cm/pixels/meanstd.pkl', 'rb'))
+except FileNotFoundError:
+    NORM = None
+    pass
 
 if __name__ == '__main__':
     pass
